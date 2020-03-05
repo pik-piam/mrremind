1698d2291ee9dd63c19bdb449bc9e5c2ae1ea13d
FeBeier <felicitas.beier@pik-potsdam.de>
2020-03-05 18:31:55 +0100

    added airrig to readLPJmL5

--- R/readLPJmL5.R
+++ R/readLPJmL5.R
@@ -1,8 +1,8 @@
 #' @title readLPJmL5
 #' @description Read LPJmL 5 content
-#' @param subtype Switch between diffrent input
+#' @param subtype Switch between different input
 #' @return List of magpie objects with results on cellular level, weight, unit and description.
-#' @author Kristine Karstens
+#' @author Kristine Karstens, Abhijeet Mishra, Felicitas Beier
 #' @seealso
 #' \code{\link{readLPJmL4}},
 #' \code{\link{readLPJ}}
@@ -21,7 +21,7 @@ readLPJmL5 <- function(subtype="CRU4p02.soilc"){
 
   if(grep("\\.",subtype)){
     
-    subtype     <- strsplit(gsub("_","/" ,subtype), split="\\.")
+    subtype     <- strsplit(subtype, split="\\.")
     folder      <- unlist(subtype)[1]
     subtype     <- unlist(subtype)[2]
   } else {
@@ -37,7 +37,7 @@ readLPJmL5 <- function(subtype="CRU4p02.soilc"){
              alitfallc       = "alitfallc_natveg.bin",
              alitfalln       = "alitfalln_natveg.bin",
              harvest         = "pft_harvest_lai.unlimN.pft.bin",
-             irrig           = "cft_airrig_lai.unlimN.pft.bin",
+             irrig           = "cft_airrig_lai.pft.bin",
              sdate           = "sdate_lai.unlimN.bin",
              hdate           = "hdate_lai.unlimN.bin",
              transpiration   = "mtransp_natveg.bin",
@@ -58,10 +58,10 @@ readLPJmL5 <- function(subtype="CRU4p02.soilc"){
 
   if(subtype%in%c("soilc","litc","vegc","alitfallc","alitfalln")){
 
-    start_year  <- start_year           #Start year of data set
-    years       <- years                #Vector of years that should be exported
+    start_year  <- start_year           # Start year of data set
+    years       <- years                # Vector of years that should be exported
     nbands      <- 1                    # Number of bands in the .bin file
-    avg_range   <- 1                    #Number of years used for averaging
+    avg_range   <- 1                    # Number of years used for averaging
 
     x <- readLPJ(
       file_name=path(folder,file_name),
@@ -77,10 +77,10 @@ readLPJmL5 <- function(subtype="CRU4p02.soilc"){
 
   } else if(grepl("*date*", subtype)){
 
-    start_year  <- start_year           #Start year of data set
-    years       <- years                #Vector of years that should be exported
-    nbands      <- 24                    # Number of bands in the .bin file
-    avg_range   <- 1                    #Number of years used for averaging
+    start_year  <- start_year           # Start year of data set
+    years       <- years                # Vector of years that should be exported
+    nbands      <- 24                   # Number of bands in the .bin file
+    avg_range   <- 1                    # Number of years used for averaging
 
     x <- readLPJ(
       file_name=path(folder,file_name),
@@ -97,10 +97,10 @@ readLPJmL5 <- function(subtype="CRU4p02.soilc"){
 
   } else if(subtype%in%c("soilc_layer")){
 
-    start_year  <- start_year           #Start year of data set
-    years       <- years                #Vector of years that should be exported
+    start_year  <- start_year           # Start year of data set
+    years       <- years                # Vector of years that should be exported
     nbands      <- 5                    # Number of bands in the .bin file
-    avg_range   <- 1                    #Number of years used for averaging
+    avg_range   <- 1                    # Number of years used for averaging
 
     x <- readLPJ(
       file_name=path(folder,file_name),
@@ -118,10 +118,10 @@ readLPJmL5 <- function(subtype="CRU4p02.soilc"){
 
   } else if(grepl("m *", subtype)){
 
-    start_year  <- start_year          #Start year of data set
-    years       <- years               #Vector of years that should be exported
-    nbands      <- 12                   #Number of bands in the .bin file
-    avg_range   <- 1                    #Number of years used for averaging
+    start_year  <- start_year         # Start year of data set
+    years       <- years              # Vector of years that should be exported
+    nbands      <- 12                 # Number of bands in the .bin file
+    avg_range   <- 1                  # Number of years used for averaging
 
     x <- readLPJ(
       file_name=path(folder,file_name),
@@ -135,7 +135,7 @@ readLPJmL5 <- function(subtype="CRU4p02.soilc"){
     x <- x*unit_transform
 
     if(grepl("layer", subtype)){
-      subtype     <- gsub("_", "\\.", subtype)                     # Expand dimension to layers
+      subtype     <- gsub("_", "\\.", subtype)       # Expand dimension to layers
       getNames(x) <- paste0(subtype,".",getNames(x))
       getSets(x)[4:6]  <- c("data" ,"layer","month")
     } else{
@@ -145,10 +145,10 @@ readLPJmL5 <- function(subtype="CRU4p02.soilc"){
 
   } else if(grepl("*harvest*", subtype)){
 
-    start_year  <- start_year           #Start year of data set
-    years       <- years                #Vector of years that should be exported
-    nbands      <- 32                    # Number of bands in the .bin file
-    avg_range   <- 1                    #Number of years used for averaging
+    start_year  <- start_year           # Start year of data set
+    years       <- years                # Vector of years that should be exported
+    nbands      <- 32                   # Number of bands in the .bin file
+    avg_range   <- 1                    # Number of years used for averaging
 
     x <- readLPJ(
       file_name=path(folder,file_name),
@@ -163,7 +163,33 @@ readLPJmL5 <- function(subtype="CRU4p02.soilc"){
     x <- collapseNames(as.magpie(x))
     x <- x*yield_transform
 
+  } else if(subtype%in%c("irrig")){
+    
+    start_year  <- start_year           # Start year of data set
+    years       <- years                # Vector of years that should be exported
+    nbands      <- 32                   # Number of bands in the .bin file
+    avg_range   <- 1                    # Number of years used for averaging
+    
+    x <- readLPJ(
+      file_name=path(folder,file_name),
+      wyears=years,
+      syear=start_year,
+      averaging_range=avg_range,
+      bands=nbands,
+      soilcells=TRUE,
+      ncells=67420)
+    
+    x <- collapseNames(as.magpie(x))
+    # Transform units (transform from: mm per year = liter per m^2 transform to: m^3 per ha)
+      # 1 000 liter   = 1 m^3
+      # 10 000 m^2    = 1 ha
+      # 1 liter/m^2   = 10 m^3/ha
+      # -> mm/yr * 10 = m^3/ha
+    irrig_transform <- 10
+    x[,,"irrigated"] <- x[,,"irrigated"]*irrig_transform # units are now: m^3 per ha per yr
+    
   } else {stop(paste0("subtype ",subtype," is not existing"))}
 
   return(x)
+
 }
