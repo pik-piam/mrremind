fff773afaaa4c642ca59dfc295dd74463527a1f9
emolinab <mbacca@pik-potsdam.de>
2020-03-12 13:42:22 +0100

    calcAWMSconfShr scenarios based on input rev

--- R/calcAWMSconfShr.R
+++ R/calcAWMSconfShr.R
@@ -1,13 +1,13 @@
 #' @title calcAWMSconfShr
 #' @description calculates the share of manure managed in different animal waste management systems in confinements. Starting with IPCC 2005 values, turning into scenarios for the future.
-#'
+#' @param rev revision number of moinput run
 #' @return List of magpie objects with results on country level, weight on country level, unit and description.
 #' @author Benjamin Leon Bodirsky. Modifications by Edna J. Molina Bacca
 #' @seealso
 #' \code{\link{calcAWMSconfShrPast}}
 #' @examples
-#' 
-#' \dontrun{ 
+#'
+#' \dontrun{
 #' calcOutput("AWMSconfShr")
 #' }
 #' @importFrom magpiesets findset
@@ -19,19 +19,19 @@ calcAWMSconfShr<-function(rev=0.1){
   past<-findset("past")
   out<-calcOutput("AWMSconfShrPast",aggregate = FALSE)
   weight2<-collapseNames(calcOutput("Excretion",aggregate = FALSE)[,past,"confinement"][,,"nr"])
-  
+
   out<-add_dimension(toolHoldConstantBeyondEnd(out),dim = 3.1,nm = "constant")
   weight2<-toolHoldConstantBeyondEnd(weight2)
-  
+
   #dairy_cows: less daily spread, more liquid, slurry and solid
-  
+
   systems_smallscale<-c("daily_spread","pit_short")
   systems_both<-c("drylot","other")
   systems_industrial<-c("lagoon","liquid_slurry","solid_storage","pit_long","digester")
-  
-  #The order of the  digester and daily_spread and traditional parameters correspond to the years_aim order 
+
+  #The order of the  digester and daily_spread and traditional parameters correspond to the years_aim order
   #2020 target values are global averages of each category for year 2010
-  
+
   out<-toolAWMSScenarioCreation(
     name="ssp1",
     start_year="y2010",
@@ -136,12 +136,12 @@ calcAWMSconfShr<-function(rev=0.1){
         ),out)
   }
 
-  
+
   return(list(x=out,
               weight=weight2,
               unit="share",
               description="share of excreted nitrogen within stables excreted in which awms",
               min=0,
               max=1)
-  ) 
+  )
 }
