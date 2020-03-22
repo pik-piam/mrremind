a3d150c4e044cb9890e655ee45acfef216166e40
emolinab <mbacca@pik-potsdam.de>
2020-03-22 21:26:33 +0100

    toolHarmonized2Baseline

--- R/toolHarmonize2Baseline.R
+++ R/toolHarmonize2Baseline.R
@@ -1,9 +1,9 @@
 #' toolHarmonize2Baseline
 #'
 #' @param x magclass object that should be set on baseline
-#' @param base nagclass object for baseline
+#' @param base magclass object for baseline
 #' @param ref_year Reference year
-#' @param limited if TRUE, for an underestimated baseline the signal is limited to he additive term rather than the multiplicative factor
+#' @param limited if TRUE, for an underestimated baseline the signal is limited to the additive term rather than the multiplicative factor
 #'                if FALSE, x will be put on the baseline as a pure multiplicative factor
 #' @param hard_cut Switch to TRUE for data that can not be harmonized, but have to be glued together
 #'
@@ -14,85 +14,80 @@
 
 toolHarmonize2Baseline <- function(x, base, ref_year, limited=TRUE, hard_cut=FALSE){
   
+  
   if(!is.magpie(x) | !is.magpie(base)) stop("Input is not a MAgPIE object, x has to be a MAgPIE object!")
   
   # check if years are overlapping and refs is part of both time horizons
   
-  
   # set years
-  years       <- union(getYears(x), getYears(base))
-  till_ref    <- getYears(base, as.integer=TRUE)
+  years       <- union(getYears(x,as.integer = TRUE), getYears(base,as.integer = TRUE))
+  till_ref    <- getYears(base,as.integer=TRUE)
   till_ref    <- till_ref[till_ref <= ref_year]
-  after_ref   <- getYears(x, as.integer=TRUE)
+  after_ref   <- getYears(x,as.integer=TRUE)
   after_ref   <- after_ref[after_ref > ref_year]
   
   
   #check if x and base are identical in dimension except time
+  if(getCells(x)!=getCells(base) | (sort(getNames(x))!=sort(getNames(base)))) stop("Dimensions of the MAgPIE objects do not match!")
   
-  
-  # create new magpie object with full time horizon
-  all         <- new.magpie(getCells(x), years, getNames(x))
+  #get the names of the dimensions
+  cells<-getCells(x)
+  dataNames<-getNames(x)
   
   # convert to arrays to speed up performance
-  all  <- as.array(all)
-  x    <- as.array(x)
-  base <- as.array(base)
+  x    <- as.data.frame(magpiesort(x))
+  base <- as.data.frame(magpiesort(base))
+  all<-merge(base,x,by=c("Cell","Region","Year","Data1"),all=TRUE)
+  all<-subset(all,Year %in% c(till_ref,after_ref))
+  colnames(all)<-c("Cell","Region","Year","Data1","Base","X")
   
-  # from start until ref_year, use the corresponding ref value
-  all[,till_ref,]      <- base[,till_ref,]
+  # Prepares the data frame that is going to be used in the operations
+  #subsets of the reference years
+  X_ref<-subset(x,Year==ref_year)
+  Base_ref<-subset(base,Year==ref_year)
+  
+  #merges all values to their suitable reference year and creates a new column with the appropriate reference value
+  all<-all %>% left_join(Base_ref,by=c("Cell","Region","Data1"))
+  all<-all %>% left_join(X_ref,by=c("Cell","Region","Data1"))
+  all$Year.y<-NULL #drops the repeated columns
+  all$Year<-NULL #drops the repeated columns
+  names(all)[names(all)=="Value.x"]<-"Base_ref" #renames the new columns
+  names(all)[names(all)=="Value.y"]<-"X_ref" #renames the new columns
+  
+  all<-all %>% mutate(lambda=sqrt(X_ref/Base_ref)) #calculates and creates a new column for lambda
   
   if(hard_cut){
     
-    ###########################################
-    ### Use GCM data after historical data  ###
-    ### from reference year +1 on           ###
-    ###########################################  
+    #creates a new variable for the hard cut scenario harmonization
+    all<-all %>% mutate(Harmonized=ifelse(Year.x %in% till_ref,Base,X))
     
-    all[,after_ref,] <- x[,after_ref,]
+  } else if (limited==FALSE){
     
-  } else {
+    #creates a new variable for the limited==FALSE scenario harmonization  
+    all<-all %>% mutate(Harmonized=ifelse(Year.x %in% till_ref,Base,
+                                          ifelse((X_ref==0),0,Base_ref*X/X_ref)))
+  }else{
     
-    ###########################################
-    ### Use DELTA-approach to put signal of ###
-    ### GCM data on historical observation  ###
-    ### data from reference year +1 on      ###
-    ###########################################
+    all<-all %>% mutate(Harmonized=ifelse(Year.x %in% till_ref,Base,
+                                          ifelse(Base_ref<=X_ref,
+                                                 ifelse(X_ref==0,0,Base_ref*X/X_ref),### LAMBDA EQUAL 1
+                                                 ifelse(X_ref==0,X+Base_ref,Base_ref*(1+((X-X_ref)/Base_ref)*(Base_ref/X_ref)**lambda))### LAMBDA UNEQUAL 1
+                                          )
+    )
+    )
     
-    for (d1 in 1:dim(all)[1]) {
-      for (d3 in 1:dim(all)[3]) {
-        
-        if((base[d1, ref_year, d3] <= x[d1, ref_year, d3]) | limited==FALSE){ 
-          ### LAMBDA EQUAL 1
-          # unlimited, relative case for non underestimatet baseline in reference year
-          
-          if(x[d1, ref_year, d3] == 0){ 
-            all[d1, after_ref, d3] <- 0                                                                     # for zero baselines
-          } else {
-            all[d1, after_ref, d3] <- base[d1, ref_year, d3] * (x[d1, after_ref, d3] / x[d1, ref_year, d3]) # for non zero baselines
-          } 
-          
-        } else {
-          ### LAMBDA UNEQUAL 1
-          # limitedcase for underestimatet baseline in reference year
-          
-          if(x[d1, ref_year, d3] == 0){
-            all[d1, after_ref, d3] <- base[d1, ref_year, d3] + x[d1, after_ref, d3]                         # for zero baselines
-          } else {
-            lambda <- sqrt( x[d1, ref_year, d3] / base[d1, ref_year, d3] )                                  # for non zero baselines
-            
-            all[d1, after_ref, d3] <- base[d1, ref_year, d3] * 
-              (1 + (x[d1, after_ref, d3] - x[d1, ref_year, d3])/base[d1, ref_year, d3] * 
-                 (base[d1, ref_year, d3]/x[d1, ref_year, d3]) ** lambda )
-          } 
-        }
-      }
-    }
+    
+    
   }
-
-  #check for nans
-  if(any(all[is.infinite(all) | is.nan(all) | is.na(all)])) stop("Data containing inconsistencies.")
   
-  out <- as.magpie(all)
+  all_final<-all[,c("Cell","Year.x","Data1","Harmonized")]
+  colnames(all_final)<-c("Cell","Year","Data1","Value")
+  all_final <- all_final[order(all_final$Year),]
+  
+  #check for nans
+  if(any(is.na(all_final$Value))) stop("Data containing inconsistencies.")
+  
+  out <- toolCell2isoCell(as.magpie(all_final,spatial=1)) 
   
   return(out)
 }
