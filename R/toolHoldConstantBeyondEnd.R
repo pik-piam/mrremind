5732e7e1ec3486c0dde6607a154d79d17bef2d9f
Jan Philipp Dietrich <dietrich@pik-potsdam.de>
2020-04-02 15:55:48 +0200

    toolHoldConstantBeyondEnd now based on mstools::toolHoldConstant

--- R/toolHoldConstantBeyondEnd.R
+++ R/toolHoldConstantBeyondEnd.R
@@ -5,14 +5,9 @@
 #' @return MAgPIE object with completed time dimensionality.
 #' @author Benjamin Leon Bodirsky
 #' @importFrom magpiesets findset
+#' @importFrom mstools toolHoldConstant
 #' @export
 
 toolHoldConstantBeyondEnd<-function(x){
-  years <- findset("time")
-  missingyears <- setdiff(years,getYears(x))
-  if(length(missingyears)==0) return(x)
-  out <- add_columns(x = x,addnm = missingyears,dim = 2.1)
-  lastyear <- paste0("y",max(getYears(x,as.integer = TRUE)))
-  out[,missingyears,] <- setYears(out[,lastyear,],NULL)
-  return(out)
+  return(toolHoldConstant(x,years=findset("time")))
 }
