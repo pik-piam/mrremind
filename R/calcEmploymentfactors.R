500be285b11744d28895a548a2ea07360978b693
amnmalik <amalik@pik-potsdam.de>
2020-06-03 14:37:19 +0200

    move from mrplayground to mrremind

---
+++ R/calcEmploymentfactors.R
@@ -0,0 +1,146 @@
+#' @title calcEmploymentfactors
+#' @description Employment factors for different technologies and activities. For all activities except Fuel_supply, units
+#' are Full Time Equivalent (FTE)/MW. For Fuel supply, units are Jobs/PJ
+#' @author Aman Malik
+#' @param improvements Either "None", CEEW", "Dias" or "Dias+CEEW". The latter three are "improvements" over only Rutovitz (None).
+
+calcEmploymentfactors <- function(improvements){
+
+  rutovitz_common <- function(){
+    # Common data for Rutovitz
+    mapping <- toolMappingFile(type = "regional",name = "regionalmappingWEO2014.csv",readcsv = T)
+    mapping_remind <- toolMappingFile(type = "regional",name = "regionmappingH12.csv",readcsv = T)
+    colnames(mapping) <- c("region","country")
+    mapping$country <- toolCountry2isocode(mapping$country)  
+    
+    oecd <- c("OECD Europe","OECD Americas","OECD Asia Oceania")
+    oecd_con <- mapping[mapping$region %in% oecd,]$country
+    # all countries not in OECD (difference taken between countries listed in remindH12 mapping file and 
+    # IEA region/country definition assumed in Rutovitz et al.)
+    non_oecd <- setdiff(mapping_remind$CountryCode,oecd_con)
+
+    x1 <- readSource(type = "Rutovitz2015",subtype = "oecd_ef")# EFs for OECD countries
+    x2 <- readSource(type = "Rutovitz2015",subtype = "regional_mult")# regional multipliers for non-OECD countries
+   
+     # countries for which regional multiplier is 0. There are actually no "0" values, but some countries are left out
+    # by Rutovitz which automatically get 0 value after the end of the convert function.
+        noval_reg <- getRegions(x2)[which(x2[,2015,]==0)]
+    for (i in noval_reg){
+      x2[i,,] <- as.numeric(x2["GHA",,])# all those countries with no regional     # multipliers get values for Africa (here Ghana taken as representative)
+      
+    }
+    x3 <- readSource(type = "Rutovitz2015",subtype = "regional_ef")# EFs for specific regions and techs
+    x4 <- readSource(type = "Rutovitz2015",subtype = "coal_ef")# EFs for coal fuel supply
+    x5 <- readSource(type = "Rutovitz2015",subtype = "gas_ef")# EFs for gas fuel supply
+  
+    # Step 1: Give all non-oecd countries, oecd ef values for 2015
+    for (i in non_oecd){
+    x1[i,,] <- as.numeric(x1["DEU",,]) # arbitary OECD country value
+    }
+    #x1[non_oecd,,] <- 1 # replacing 0 (value) with 1 for multiplication later
+    # Step 2: Multiply all non-oecd values by a regional multiplier
+    #  for 2015 (for oecd countries, this factor is 1)
+    x1[non_oecd,2015,] <- x1[non_oecd,2015,] * setNames(x2[non_oecd,2015,],nm = NULL) 
+  
+  # Step 3: overwrite efs (values) for 2015 for countries for which there is
+  # better data
+  
+  # 3a.Replacing EFs in x1 for common elements in x1 and x3, only for 2015
+    for (i in getRegions(x3)){
+      for (j in getNames(x3)){
+        if (x3[i,,j]!=0) # for all non-zero values in x3, replace data in x1 with x3
+        (x1[i,2015,j] <- x3[i,2015,j])
+      }
+    }
+  
+  
+  # 3b. Replacing coal fuel supply EFs in x1 with those of x4, only for 2015
+      for (i in getRegions(x4)){
+        for (j in getNames(x4)){
+          if (x4[i,,j]!=0)
+            (x1[i,2015,j] <- x4[i,2015,j])
+        }
+      }
+  
+  
+  # 3c. Replacing gas EFs in x1 with those of x5,only or 2015
+      for (i in getRegions(x4)){
+        for (j in getNames(x5)){
+          if (x5[i,,j]!=0)
+            (x1[i,"y2015",j] <- x5[i,"y2015",j])
+        }
+      }
+  # Step 4: For future values (2020 and 2030), the regional multiplication factors 
+  # which are provided for 2020 and 2030 are normalised to 2015 values.
+  x2[,"y2020",] <- x2[,"y2020",]/setYears(x2[,"y2015",],NULL)
+  x2[,"y2030",] <- x2[,"y2030",]/setYears(x2[,"y2015",],NULL)
+
+  # Step 5: Future values found by multiplying with 2015 values
+  x1[non_oecd,2020,] <- setYears(x1[non_oecd,2015,],NULL) * setNames(x2[non_oecd,2020,],NULL) # multiplying by regional multiplier
+  x1[non_oecd,2030,] <- setYears(x1[non_oecd,2015,],NULL) * setNames(x2[non_oecd,2030,],NULL) # multiplying by regional multiplier
+  # For OECD countries, the multiplier is 1 in 2020 and 2030
+  x1[oecd_con,"y2020",]<- as.numeric(x1[oecd_con,"y2015",]) 
+  x1[oecd_con,"y2030",]<- as.numeric(x1[oecd_con,"y2015",]) 
+  return (x1)
+  } 
+  
+  ceew <- function(){  
+  x6 <- readSource(type = "CEEW",subtype = "Employment factors") # EFs for India from CEEW
+  # Replacing Large Hydro with Hydro (Note!!)
+  getNames(x6) <- gsub(x = getNames(x6),pattern = "Large Hydro",replacement = "Hydro")
+  
+  # overwriting Rutovitz (x1) values with "better" or region-specific data
+  # from CEEW (India) for some common variables
+  # note than all years from x1 (2015,2020,2030)  get CEEW values
+  com_var <- getNames(x1)[getNames(x1) %in% getNames(x6)]
+  x1["IND",,com_var] <- x6["GLO",,com_var]
+  return (x1)
+  }
+  dias <- function(){
+    x7 <- readSource(type = "Dias", subtype = "Employment factors") # EFs for coal and coal mining
+    # using Dias et al. data, regions with non-zero values 
+    regs <- getRegions(x7)[which(x7>0)]
+    # Overwriting all efs in x1 for which better data exists in x7
+    # for all years
+    x1[regs,,getNames(x7)] <- setYears(x7[regs,,],NULL)
+    return (x1)
+  }
+  
+  if (improvements=="None")
+    {
+    x1 <- rutovitz_common()
+  }
+  if(improvements=="CEEW")
+    {
+    x1 <-  rutovitz_common()
+    x1 <- ceew()
+    
+  }
+  
+  if (improvements=="Dias")
+    {
+    x1 <- rutovitz_common()
+    x1 <- dias()
+  
+  }
+  
+  if (improvements=="Dias+CEEW")
+  {
+    x1 <- rutovitz_common()
+    x1 <- ceew()  
+    x1 <- dias()
+  }
+  
+  # using gdp per capita fpr regional aggregation
+  gdp <-   calcOutput("GDPppp",   years=2015, aggregate = F)
+  gdp <- gdp[,,"gdp_SSP2"]
+  
+  pop <-  calcOutput("Population", years=2015,aggregate = F) 
+  pop <- pop[,,"pop_SSP2"]
+  
+  gdppercap <- gdp/pop
+
+return(list(x=x1, weight=gdp,  unit="FTE/MW or FTE/PJ",
+            description="Employment factors"))
+}
+
---
+++ R/readRutovitz2015.R
@@ -0,0 +1,147 @@
+#' Employment factors for various power production technologies from Rutovitz et al. 2015
+#' @author Aman Malik
+#' @importFrom  tidyr gather_
+#' @importFrom dplyr rename add_row filter_ mutate_ select_ mutate_at left_join filter mutate
+#' @importFrom readr read_csv
+#' @importFrom mgsub mgsub
+#' @return magpie object of emplyoment factors for different technologies in Jobs/MW
+#' @param subtype Either "oecd_ef","regional_ef","coal_ef","gas_ef", "regional_mult
+
+
+readRutovitz2015 <- function(subtype){
+  
+  CI <- NULL
+  Fuel_supply <- NULL
+  Manf <- NULL
+  OM <- NULL
+  activity <- NULL
+  region <- NULL
+  tech <- NULL
+  duration <- NULL
+  Productivity <- NULL
+  Year <- NULL
+  notes <- NULL
+  value <- NULL
+  vars <- NULL
+  if (subtype == "oecd_ef")
+    {
+    
+    input <- read_csv("oecd_ef.csv",na = "") %>% 
+      # rename_(tech=~"X1",duration=~`Construction times`,CI=~`Construction/ Installation`,
+      #         Manf=~`Manufacturing`,OM=~`Operations & Maintenance`, Fuel_supply=`Fuel – PRIMARY ENRGY DEMAND\nEnergy Demand`) %>% 
+      rename(tech=1,duration=2,CI=3,Manf=4,OM=5, Fuel_supply=6) %>% 
+      filter_(~!is.na(tech)) %>% 
+      add_row(tech="CoalHP",duration=5,CI=11.2,Manf=5.4,OM=0.14*1.5,Fuel_supply="Regional") %>% 
+      add_row(tech="GasHP",duration=2,CI=1.3,Manf=0.93,OM=0.14*1.5,Fuel_supply="Regional") %>% 
+      add_row(tech="BiomassHP",duration=2,CI=14,Manf=2.9,OM=1.5*1.5,Fuel_supply=29.9) %>% 
+      add_row(tech="Oil",CI=1.3,Manf=0.93,OM=0.14,Fuel_supply="Regional") %>% # oil EF= Gas EF
+      filter_(~!tech %in% grep("Ocean|decommissioning|small|Oil|heat",
+                             x =tech,value=T)) %>%  # removing 
+    mutate_(tech=~mgsub::mgsub(tech, c("Hydro-large", "Wind onshore","Solar Photovoltaics","Solar thermal"),
+                             c("Hydro", "Wind","Solar|PV","Solar|CSP"))) %>%  ## renaming techs
+     # mutate(CI=as.numeric(CI)) %>% 
+      #mutate(Manf=as.numeric(Manf)) %>% 
+    mutate_at(vars(CI,Manf,OM,duration),as.numeric) %>% 
+      #mutate_(CI=~CI/duration) %>% # dividing employment intensity by construction period
+      #mutate_(Manf=~Manf/duration) %>% 
+      select_(~-duration) %>% 
+      gather_(gather_cols= c("CI","Manf","OM","Fuel_supply","value"),key_col = "activity",value_col= "value") %>% 
+      mutate(value=ifelse(value == "Regional",0,value))  %>% 
+      #mutate_(unit=ifelse(activity %in% c("CI","Manf"), yes = "Job-years/MW", no = ifelse(activity=="Fuel_supply","Jobs/PJ","Jobs/MW"))) %>% # adding units column
+     # filter_( ~tech!="Nuclear") %>% 
+      #mutate(life_span = ifelse(tech=="Coal",40,
+       #                         ifelse(tech=="Gas",30,
+        #                               ifelse(tech %in% c("Nuclear","Hydro"),60,
+         #                                     ifelse(tech %in% c("Solar|PV","Solar|CSP","Wind"),25,30))))) %>% 
+      mutate_at(vars(tech,activity),as.factor) %>% 
+      mutate(value=as.numeric(value))
+    
+   
+    
+      x <- as.magpie(input,temporal=NULL,spatial=NULL,datacol=3)
+    return (x)
+  }
+  
+
+  if (subtype == "regional_ef")
+    {
+    
+  const <- data.frame(tech=c("Nuclear","Biomass","Hydro","Wind","Solar|PV","Solar|CSP","Wind offshore"),
+                        duration=c(10,2,2,2,1,2,4))
+  input <- read_csv("regional_ef.csv",na="") %>%
+    rename(tech=1,region=2,CI=3,Manf=4,OM=5,Fuel_supply=6) %>%
+    filter(!is.na(tech)) %>%
+    mutate_at(vars(tech,region),as.character) %>%
+    mutate_at(vars(CI,Manf,OM,Fuel_supply),as.numeric) %>%
+    mutate(tech=mgsub::mgsub(tech, c("Hydro-large", "Wind-onshore","Solar PV","Solar Thermal power","Wind-offshore"),
+                                   c("Hydro", "Wind","Solar|PV","Solar|CSP","Wind offshore"))) %>%
+    gather_(gather_cols= c("CI","Manf","OM","Fuel_supply","value"),key_col = "activity",value_col= "value") %>% 
+    filter(!grepl("average",region))%>% 
+    mutate(region=mgsub::mgsub(region,c("OECD North America","OECD Pacific"),
+                        c("OECD Americas","OECD Asia Oceania"))) %>% 
+    na.omit() %>% 
+    left_join(const,by="tech") %>% 
+    #mutate(region=ifelse(region=="OECD North America","OECD Americas",region))
+    mutate(value=ifelse(!activity %in% c("Fuel_supply","OM"),value/duration,value)) %>% 
+    select(region,tech,activity,value)
+    
+
+  x <- as.magpie(input,spatial=1,temporal=NULL,datacol=4)
+
+  return (x)
+}
+
+  if(subtype=="coal_ef")
+    {
+   input <- read_csv("coal_ef.csv",na="") %>% 
+     select(-Year,-Productivity) %>% 
+     rename(region=1,value=2) %>% 
+     filter(!is.na(region)) %>% 
+     mutate(value=ifelse(region=="Middle East",39.7,value)) %>% 
+     filter(!grepl("World",region)) %>% 
+     mutate(region=ifelse(region=="Eastern Europe/Eurasia","Eurasia",region)) %>% 
+     mutate(tech="Coal") %>% 
+     mutate(activity="Fuel_supply") %>% 
+     mutate(value=as.numeric(value))
+   input <- input[,c("region","tech","activity","value")]
+   
+   x <- as.magpie(input,temporal=NULL,datacol=4,spatial=1)
+   
+   return (x)
+ }
+
+  if(subtype=="gas_ef")
+    {
+  input2 <- data.frame(region=c("India","Latin America","Developing Asia","Middle East"),value=15.1)
+  
+  input <- read_csv("gas_ef.csv",na="") %>% 
+    rename(region=1,value=2,notes=3) %>% 
+    select(-notes)  %>% 
+    rbind(input2) %>% 
+    filter(!is.na(value)) %>% 
+    filter(!grepl("World",region))  %>% 
+    mutate(region=ifelse(region=="Eastern Europe/Eurasia","Eurasia",region)) %>% 
+    mutate(tech="Gas") %>% 
+    mutate(activity="Fuel_supply")
+  
+  input <- input[,c("region","tech","activity","value")]
+  
+  x <- as.magpie(input,temporal=NULL,datacol=4,spatial=1)
+  
+  return(x)  
+}  
+  if(subtype=="regional_mult"){
+       input <- read_csv("regional_mult.csv",na="") %>% 
+    rename(region=1) %>% 
+    gather(2:4,key="year",value="value") %>% 
+    mutate(region=ifelse(region=="Eastern Europe/Eurasia","Eurasia",region))
+  
+  x <- as.magpie(input,spatial=1,temporal=2)
+  
+  return (x)
+  
+  }
+  
+}
+
+   
