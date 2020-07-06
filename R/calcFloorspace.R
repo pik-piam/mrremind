436a9e87bcfbab85b4776c09cb662c8b3e175657
Antoine Levesque <levesque@pik-potsdam.de>
2020-07-06 17:55:19 +0200

    added a dimension to floorspace projections (except for the output of calcFloorspace)

--- R/calcFloorspace.R
+++ R/calcFloorspace.R
@@ -2,7 +2,7 @@
 calcFloorspace <- function() {
   
   data <- readSource("EDGE", subtype = "Floorspace") 
-  
+  data <- collapseNames(data[,,"buildings"])
   
   return(list(x=data,weight=NULL, unit = "million m2", description = "Buildings floorspace"))
 }
