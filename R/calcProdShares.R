91f6eff1d53935299c766e460584f8d1c719ec52
amnmalik <amalik@pik-potsdam.de>
2020-09-04 16:26:44 +0300

    added battery storage job intensities and some EU-specific intensities for O&M. bugfix in calcProdShares

--- R/calcProdShares.R
+++ R/calcProdShares.R
@@ -1,8 +1,13 @@
+#' Shares of world manufacture for spv modules and wind turbines for 2018 and 2019
+#' @author Aman Malik
+#' @return magpie object with REMIND-aggregated region
+
+
 calcProdShares <- function(){
   x <- readSource(type = "ProdShares")
   
 return(list(x=x, 
-            weight=x,
+            weight=NULL,
               unit="",
             description= "share of world manufacture for solar pv modules and wind turbines"))
   
