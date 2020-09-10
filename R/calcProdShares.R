b919b2e876e067bbfaf06bb780def0735b10b98c
amnmalik <amalik@pik-potsdam.de>
2020-09-10 16:57:09 +0300

    added regional multipliers from Ram et al., employment factors for battery storage from the same study, and changed factors for EUR for OM stage from study by Fragkos.

--- R/calcProdShares.R
+++ R/calcProdShares.R
@@ -1,10 +1,10 @@
-#' Shares of world manufacture for spv modules and wind turbines for 2018 and 2019
+#' @description Shares of world manufacture for spv modules and wind turbines for 2018 and 2019
 #' @author Aman Malik
 #' @return magpie object with REMIND-aggregated region
 
 
 calcProdShares <- function(){
-  x <- readSource(type = "ProdShares")
+  x <- readSource(type = "ProdShares",convert = F)
   
 return(list(x=x, 
             weight=NULL,
---
+++ R/convertRam.R
@@ -0,0 +1,29 @@
+#' Regional multiliers from Ram et al. , 2018
+# x <- readSource("Ram",convert=F)
+#' @param x MAgPIE object to be converted
+
+convertRam <- function(x){
+  mapr <- toolMappingFile(type = "regional",name = "regionmappingH12.csv",readcsv = T)
+  
+  #x <- readSource(type = "Ram",convert=F)
+  x_final <- new.magpie(cells_and_regions = mapr$CountryCode,years = seq(2015,2050,5),names = "value")
+  
+  # mapping REMIND regions to regions from Ram et al.
+  
+  x_final[mapr[mapr$RegionCode=="EUR",]$CountryCode,,] <- x["Europe",,]
+  x_final[mapr[mapr$RegionCode=="NEU",]$CountryCode,,] <- x["Europe",,]
+  x_final[mapr[mapr$RegionCode=="LAM",]$CountryCode,,] <- x["South America",,]
+  x_final[mapr[mapr$RegionCode=="MEA",]$CountryCode,,] <- x["MENA",,]
+  x_final[mapr[mapr$RegionCode=="SSA",]$CountryCode,,]  <- x["Sub-Saharan Africa",,]
+  x_final[mapr[mapr$RegionCode=="CHA",]$CountryCode,,] <- x["Northeast Asia",,]
+  x_final[mapr[mapr$RegionCode=="JPN",]$CountryCode,,] <- x["Northeast Asia",,]
+  x_final[mapr[mapr$RegionCode=="REF",]$CountryCode,,] <- x["Eurasia",,]
+  x_final[mapr[mapr$RegionCode=="OAS",]$CountryCode,,] <- x["Southeast Asia",,]
+  x_final[mapr[mapr$RegionCode=="USA",]$CountryCode,,] <- x["North America",,]
+  x_final[mapr[mapr$RegionCode=="OAS",]$CountryCode,,] <- x["Southeast Asia",,]
+  x_final[mapr[mapr$RegionCode=="CAZ",]$CountryCode,,] <- x["North America",,]
+  x_final[c("IND","PAK","BGD","NPL","LKA","MDV","AFG","BTN"),,] <- x["SAARC",,]
+  x <- x_final
+  return (x)
+
+}
