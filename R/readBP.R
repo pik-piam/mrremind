99a54356511ee4a79cca4e9c040d709b026823f4
amnmalik <amalik@pik-potsdam.de>
2020-10-11 17:50:57 +0200

    updated generation and capacity data from BP Statistical Review of World Energy to latest values (2019)

--- R/readBP.R
+++ R/readBP.R
@@ -11,26 +11,26 @@
 #' @importFrom reshape merge_recurse
 
 
-
-
 readBP <- function(subtype) {
-  y3 <- NULL
+  value <- NULL
   Country <- NULL
   Year <- NULL
-  filename <- c("bp-statistical-review-of-world-energy-2017-underpinning-data.xlsx")
+  filename <- c("bp-stats-review-2020-all-data.xlsx")
   # Function to tidy input data, y1 is the dataframe and y2 is the specific technology
   tidy_data <- function(y1,y2){
-    columns2remove <- c("2005-15","2016__1", "2016__2") # some columns in excel sheet converted to these forms while reading from readxl. Not required.
-    rows2remove <- c("Total|CIS|OECD|European")
+    years <- as.character(c(1900:2020)) # only columns with years required
+    rows2remove <- c("Total|OECD|European")
     colnames(y1)[1] <- "Country" # renaming column name
     y1$Country <- gsub("\\. ", " ",y1$Country) # Removing dots in country names
     y1 <- y1 %>%
-      gather(colnames(y1[1,-1]),key="Year", value=y3) %>%  # data in wide format, y3 are capacity/generation values
-      filter(!grepl(rows2remove, Country),!is.na(y3),!y3=="n/a",!Year %in% columns2remove) # removing entries with criteria columns2rmove, rows2remove, and 'n/a'
+      gather(colnames(y1[1,-1]),key="Year", value=value) %>%  
+      filter(!grepl(rows2remove, Country),!is.na(value),!value=="n/a",Year %in% years) %>% 
+      mutate(Year=as.integer(Year),value=as.numeric(value)) %>% 
+      mutate(Country=gsub(pattern = " and ",replacement = " & ",x = Country))
     
-    y1$y3 <- as.numeric(y1$y3) # change type from default (character) to numeric
-    colnames(y1)[3] <- y2
-    return(y1)
+    colnames(y1)[3] <- y2  
+    
+      return(y1)
     }
     
     
@@ -38,11 +38,11 @@ readBP <- function(subtype) {
   # Capacity Data for Wind, Solar, and Geobiomass ---------------------------
   if (subtype == "Capacity") {
     
-    y1 <- read_excel(filename, sheet ="Solar capacity" ,skip = 3)
+    y1 <- read_excel(filename, sheet ="Solar Capacity" ,range = "A4:Y69")
     data_solar <- tidy_data(y1,"Solar")
-    y1 <- read_excel(filename, sheet ="Wind capacity" ,skip = 3)
+    y1 <- read_excel(filename, sheet ="Wind Capacity",range = "A4:Z70")
     data_wind <- tidy_data(y1,"Wind")
-    y1 <- read_excel(filename, sheet ="Geothermal capacity" ,skip = 3)
+    y1 <- read_excel(filename, sheet ="Geothermal Capacity" ,range="A4:Z44")
     data_geothermal <- tidy_data(y1,"Geothermal")
     
     #Merging dataframes, accepting all values
@@ -51,25 +51,22 @@ readBP <- function(subtype) {
   } 
   # Generation data for Nuclear, Hydro, Solar, Wind, Geobiomass, Other Renewables--------
   else if(subtype== "Generation") {
-   y1 <- read_excel(filename, sheet = "Nuclear Consumption - TWh", skip = 2)
+   y1 <- read_excel(filename, sheet = "Nuclear Generation - TWh", range = "A3:BD114")
    data_nuclear <- tidy_data(y1,"Nuclear")
     
-   y1 <- read_excel(filename, sheet = "Hydro Consumption - TWh", skip = 2)
+   y1 <- read_excel(filename, sheet = "Hydro Generation - TWh", range="A3:BD114")
    data_hydro <- tidy_data(y1,"Hydro")
   
-   data_other_renewables <- read_excel(filename,  sheet = "Other renewables -TWh", skip = 2)
-   data_other_renewables <- tidy_data(y1,"Other_Renewables")
-   
-   y1 <- read_excel(filename, sheet = "Solar Consumption - TWh", skip = 2)
+   y1 <- read_excel(filename, sheet = "Solar Generation - TWh", range="A3:BD114")
    data_solar <- tidy_data(y1,"Solar")
    
-   y1 <- read_excel(filename, sheet = "Wind Consumption - TWh", skip = 2)
+   y1 <- read_excel(filename, sheet = "Wind Generation - TWh", range="A3:BD114")
    data_wind <- tidy_data(y1,"Wind")
    
-   y1 <- read_excel(filename, sheet = "Geo Biomass Other - TWh", skip = 2)
+   y1 <- read_excel(filename, sheet = "Geo Biomass Other - TWh", range="A3:BD114")
    data_geo_biomass <- tidy_data(y1,"Geo_biomass")
    
-   my_list <- list(data_wind,data_solar,data_hydro,data_geo_biomass,data_nuclear,data_other_renewables)
+   my_list <- list(data_wind,data_solar,data_hydro,data_geo_biomass,data_nuclear)
    data <- merge_recurse(my_list) # merging all datasets into one
    data <- filter(data,!grepl("\\.",data$Year))
    
@@ -78,6 +75,7 @@ readBP <- function(subtype) {
     stop("Not a valid subtype!")
   }
   x <- as.magpie(data,temporal=2,spatial=1,datacol=3) 
+  x <- magpiesort(x)
   return(x)
   
 }
