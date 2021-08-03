b75d5db692439cb23601e9cb52eaa8f619b4f36c
Pascal Führlich <pascal.fuehrlich@pik-potsdam.de>
2021-08-03 13:02:08 +0200

    reformat readEDGE

--- R/readEDGE.R
+++ R/readEDGE.R
@@ -1,79 +1,82 @@
 #' Read EDGE
-#' 
-#' Read-in an EDGE csv file as magclass object
-#' 
-#' 
-#' @return magpie object of EDGE
-#' @author Antoine Levesque
-#' @seealso \code{\link{readSource}}
-#' @param subtype FE for final energy or Capital for capital projections
+#'
+#' Load an EDGE Transport or Buildings csv/cs4r file as magclass object.
+#'
+#' @param subtype One of the possible subtypes, see default argument.
+#' @return magclass object
 #'
 #' @examples
-#' \dontrun{ a <- readSource(type="EDGE")
+#' \dontrun{
+#' x <- readSource("EDGE")
 #' }
+#' @author Antoine Levesque
+#' @seealso \code{\link{readSource}}
 #' @importFrom magclass read.magpie
-readEDGE <- function(subtype = "FE_stationary") {
-
- if (subtype == "FE_stationary"){
-  mstationary <- read.magpie("EDGE_TradMod.cs4r")
-  mstationary[is.na(mstationary)] <- 0
-  # use SSP1 data also for SSP1plus/SDP
-  mstationary_SDP <- mstationary[,,"SSP1"]
-  getNames(mstationary_SDP) <- gsub("SSP1","SDP",getNames(mstationary_SDP))
-  mstationary <- mbind(mstationary,mstationary_SDP)
-  getSets(mstationary) <- c("region", "year", "scenario", "item")
-  
-    mdata <- mstationary
- } else if (subtype == "FE_buildings") {
- 
-   mbuilding <- read.csv("EDGE_buildings_EDGE_EUR_ETP_CCoff.csv") 
-   mbuilding <- as.magpie(mbuilding)
-   # read in additional data for SSP1plus/SDP
-   mbuilding_SDP <- read.csv("EDGE_buildings_EDGE_EUR_ETP_CCoff_SSP1plus.csv") 
-   mbuilding_SDP <- as.magpie(mbuilding_SDP)
-   # rename SSP1 into SDP
-   getNames(mbuilding_SDP) <- gsub("SSP1plus","SDP",getNames(mbuilding_SDP))
-   # add data on SSP1plus/SDP to data for all other scenarios
-   mbuilding <- mbind(mbuilding,mbuilding_SDP)
-   getSets(mbuilding) <- c("region", "year", "scenario", "item")
- 
-   mdata = mbuilding  
- } else if(subtype == "Capital"){
-   mcapital = read.csv("capitalProjections.csv")
-   mcapital = as.magpie(mcapital)
-   mcapital = collapseNames(mcapital)
-   getSets(mcapital) <- c("region", "year", "scenario")
-   mdata = mcapital
-   
- } else if(subtype == "CapitalUnit"){
-   
-   mcapitalunit_cap = read.csv("capitalUnitCost_cap.csv")
-   mcapitalunit_cap$type = "cap"
-   mcapitalunit_inv = read.csv("capitalUnitCost_inv.csv")
-   mcapitalunit_inv$type = "inv"
-   mcapitalunit = rbind(mcapitalunit_cap,mcapitalunit_inv)
-   mcapitalunit = mcapitalunit[c(setdiff(colnames(mcapitalunit),"value"),"value")]
-   mcapitalunit = as.magpie(mcapitalunit,tidy = T)
-   mcapitalunit = collapseNames(mcapitalunit)
-   mdata = mcapitalunit
-   
- } else if(subtype == "Floorspace"){
-   mfloor = read.csv("EDGE_buildings_floorspace.csv")
-   mfloor = as.magpie(mfloor)
-   mfloor = collapseNames(mfloor)
-   getSets(mfloor) <- c("region", "year", "scenario", "variable")
-   mdata = mfloor
- }else if(subtype == "ES_buildings"){
-   mservices = read.csv("EDGE_buildings_services_EDGE_EUR_ETP.csv")
-   mservices = as.magpie(mservices)
-   mservices = collapseNames(mservices)
-   getSets(mservices) <- c("region", "year", "scenario")
-   mdata = mservices
- }else if(subtype == "FE_transport"){
-   mtransport <- read.magpie("EDGE_transport_data.csv", file_type="csv")
-   mtransport = collapseNames(mtransport)
-   getSets(mtransport) <- c("region", "year", "scenario")
-   mdata = mtransport
- }else { stop("valid subtypes are 'FE_stationary', 'FE_buildings', 'Floorspace','ES_buildings and 'Capital', 'FE_transport")} 
-  return(mdata)
-}  
+readEDGE <- function(subtype = c("FE_stationary", "FE_buildings", "Capital", "CapitalUnit", "Floorspace",
+                                 "ES_buildings", "FE_transport")) {
+  subtype <- match.arg(subtype)
+  switch(
+    subtype,
+    FE_stationary = {
+      mstationary <- read.magpie("EDGE_TradMod.cs4r")
+      mstationary[is.na(mstationary)] <- 0
+      # use SSP1 data also for SSP1plus/SDP
+      mstationarySDP <- mstationary[, , "SSP1"]
+      getNames(mstationarySDP) <- gsub("SSP1", "SDP", getNames(mstationarySDP))
+      mstationary <- mbind(mstationary, mstationarySDP)
+      getSets(mstationary) <- c("region", "year", "scenario", "item")
+      return(mstationary)
+    },
+    FE_buildings = {
+      mbuilding <- read.csv("EDGE_buildings_EDGE_EUR_ETP_CCoff.csv")
+      mbuilding <- as.magpie(mbuilding)
+      # read in additional data for SSP1plus/SDP
+      mbuildingSDP <- read.csv("EDGE_buildings_EDGE_EUR_ETP_CCoff_SSP1plus.csv")
+      mbuildingSDP <- as.magpie(mbuildingSDP)
+      # rename SSP1plus to SDP
+      getNames(mbuildingSDP) <- gsub("SSP1plus", "SDP", getNames(mbuildingSDP))
+      # add data on SSP1plus/SDP to data for all other scenarios
+      mbuilding <- mbind(mbuilding, mbuildingSDP)
+      getSets(mbuilding) <- c("region", "year", "scenario", "item")
+      return(mbuilding)
+    },
+    Capital = {
+      mcapital <- read.csv("capitalProjections.csv")
+      mcapital <- as.magpie(mcapital)
+      mcapital <- collapseNames(mcapital)
+      getSets(mcapital) <- c("region", "year", "scenario")
+      return(mcapital)
+    },
+    CapitalUnit = {
+      mcapitalunitCap <- read.csv("capitalUnitCost_cap.csv")
+      mcapitalunitCap$type <- "cap"
+      mcapitalunitInv <- read.csv("capitalUnitCost_inv.csv")
+      mcapitalunitInv$type <- "inv"
+      mcapitalunit <- rbind(mcapitalunitCap, mcapitalunitInv)
+      mcapitalunit <- mcapitalunit[c(setdiff(colnames(mcapitalunit), "value"), "value")]
+      mcapitalunit <- as.magpie(mcapitalunit, tidy = TRUE)
+      mcapitalunit <- collapseNames(mcapitalunit)
+      return(mcapitalunit)
+    },
+    Floorspace = {
+      mfloor <- read.csv("EDGE_buildings_floorspace.csv")
+      mfloor <- as.magpie(mfloor)
+      mfloor <- collapseNames(mfloor)
+      getSets(mfloor) <- c("region", "year", "scenario", "variable")
+      return(mfloor)
+    },
+    ES_buildings = {
+      mservices <- read.csv("EDGE_buildings_services_EDGE_EUR_ETP.csv")
+      mservices <- as.magpie(mservices)
+      mservices <- collapseNames(mservices)
+      getSets(mservices) <- c("region", "year", "scenario")
+      return(mservices)
+    },
+    FE_transport = {
+      mtransport <- read.magpie("EDGE_transport_data.csv", file_type = "csv")
+      mtransport <- collapseNames(mtransport)
+      getSets(mtransport) <- c("region", "year", "scenario")
+      return(mtransport)
+    }
+  )
+}
