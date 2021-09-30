a0270487d1b6db3b489ebbc90597bd798c0a26ca
Alois Dirnaichner <alodi@directbox.com>
2021-09-30 17:34:59 +0200

    Explicitly select EDGE-T switches when creating inputdata.

--- R/calcEDGETrData.R
+++ R/calcEDGETrData.R
@@ -16,26 +16,33 @@ calcEDGETrData <- function() {
   regionmapping2use <- infoConfig$regionmapping
   setConfig(regionmapping = "2b1450bc.csv")
   ## run EDGE-T
-  EDGETdata = lapply(c(paste0('SSP', c(1, 2, 5)), "SSP2EU", "SDP"),
-                     function(x) {
-                       generateEDGEdata(input_folder = paste0(getConfig("mainfolder"), "/sources/EDGE-T_standalone/"),
-                                        output_folder = NULL,
-                                        SSP_scen = x,
-                                        IEAbal = calcOutput("IO", subtype = "IEA_output", aggregate = TRUE),
-                                        GDP_country = {
-                                            x <- calcOutput("GDPppp", aggregate = F)
-                                            getSets(x)[1] <- "ISO3"
-                                            getSets(x)[2] <- "Year"
-                                            x
-                                          },
-                                        POP_country = {
-                                            x <- calcOutput("Population", aggregate = F)
-                                            getSets(x)[1] <- "iso2c"
-                                            x
-                                          },
-                                        trsp_incent = readSource("TransportSubsidies", convert=T),
-                                        storeRDS = FALSE)
-                       })
+  EDGETdata = lapply(list(
+    c("SSP1", "ElecEra", TRUE),
+    c("SSP2", "Mix", FALSE),
+    c("SSP5", "ConvCase", FALSE),
+    c("SSP2EU", "Mix", FALSE),
+    c("SDP", "ElecEraWise", TRUE)),
+    function(x) {
+      generateEDGEdata(input_folder = paste0(getConfig("mainfolder"), "/sources/EDGE-T_standalone/"),
+                       output_folder = NULL,
+                       SSP_scen = x[1],
+                       tech_scen = x[2],
+                       smartlifestyle = x[3],
+                       IEAbal = calcOutput("IO", subtype = "IEA_output", aggregate = TRUE),
+                       GDP_country = {
+                         x <- calcOutput("GDPppp", aggregate = F)
+                         getSets(x)[1] <- "ISO3"
+                         getSets(x)[2] <- "Year"
+                         x
+                       },
+                       POP_country = {
+                         x <- calcOutput("Population", aggregate = F)
+                         getSets(x)[1] <- "iso2c"
+                         x
+                       },
+                       trsp_incent = readSource("TransportSubsidies", convert=T),
+                       storeRDS = FALSE)
+    })
 
 
   setConfig(regionmapping = regionmapping2use)
