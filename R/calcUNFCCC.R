1514167e88d17003b650df1f27645e53def0f75b
Pascal Weigmann <pascalwe@LT944.pik-potsdam.de>
2021-11-10 12:35:46 +0100

    add additional aggregations to UNFCCC and CEDS2021

--- R/calcUNFCCC.R
+++ R/calcUNFCCC.R
@@ -37,11 +37,74 @@ calcUNFCCC <- function() {
       is.na(!!sym('value')), 0,  !!sym('value') * !!sym('conversion')),
       !!sym('REMIND') := paste0(!!sym('REMIND'),  " (", !!sym('Unit_REMIND'), ")")) %>%
     select('variable' = 'REMIND', 'region', 'year', 'value')
-  
+
   x <- aggregate(value ~ variable+region+year, x, sum) %>% 
     as.magpie() %>% 
     toolCountryFill(fill = 0)
-  
+
+  # aggregate pollutants
+  x <- add_columns(x, "Emi|CH4 (Mt CH4/yr)", dim=3.1)
+  x[,, "Emi|CH4 (Mt CH4/yr)"] <-
+    x[,, "Emi|CH4|Agriculture (Mt CH4/yr)"] +
+    x[,, "Emi|CH4|Energy (Mt CH4/yr)"] +
+    x[,, "Emi|CH4|Industrial Processes (Mt CH4/yr)"] +
+    x[,, "Emi|CH4|Land-Use Change (Mt CH4/yr)"] +
+    x[,, "Emi|CH4|Waste (Mt CH4/yr)"]
+
+  x <- add_columns(x, "Emi|CO2 (Mt CO2/yr)", dim=3.1)
+  x[,, "Emi|CO2 (Mt CO2/yr)"] <-
+    x[,, "Emi|CO2|Agriculture (Mt CO2/yr)"] +
+    x[,, "Emi|CO2|Energy (Mt CO2/yr)"] +
+    x[,, "Emi|CO2|Industrial Processes (Mt CO2/yr)"] +
+    x[,, "Emi|CO2|Land-Use Change (Mt CO2/yr)"] +
+    x[,, "Emi|CO2|Waste (Mt CO2/yr)"]
+
+  x <- add_columns(x, "Emi|N2O (kt N2O/yr)", dim=3.1)
+  x[,, "Emi|N2O (kt N2O/yr)"] <-
+    x[,, "Emi|N2O|Agriculture (kt N2O/yr)"] +
+    x[,, "Emi|N2O|Energy (kt N2O/yr)"] +
+    x[,, "Emi|N2O|Industrial Processes (kt N2O/yr)"] +
+    x[,, "Emi|N2O|Land-Use Change (kt N2O/yr)"] +
+    x[,, "Emi|N2O|Waste (kt N2O/yr)"]
+
+  # add total GHG as CO2 equivalents for sectors
+  x <- add_columns(x, "Emi|GHG|Energy (Mt CO2-equiv/yr)", dim=3.1)
+  x[,, "Emi|GHG|Energy (Mt CO2-equiv/yr)"] <-
+    x[,, "Emi|CO2|Energy (Mt CO2/yr)"] +
+    x[,, "Emi|CH4|Energy (Mt CH4/yr)"] * 28 +
+    x[,, "Emi|N2O|Energy (kt N2O/yr)"] / 1000 * 265
+
+  x <- add_columns(x, "Emi|GHG|Industrial Processes (Mt CO2-equiv/yr)", dim=3.1)
+  x[,, "Emi|GHG|Industrial Processes (Mt CO2-equiv/yr)"] <-
+    x[,, "Emi|CO2|Industrial Processes (Mt CO2/yr)"] +
+    x[,, "Emi|CH4|Industrial Processes (Mt CH4/yr)"] * 28 +
+    x[,, "Emi|N2O|Industrial Processes (kt N2O/yr)"] / 1000 * 265
+
+  x <- add_columns(x, "Emi|GHG|Agriculture (Mt CO2-equiv/yr)", dim=3.1)
+  x[,, "Emi|GHG|Agriculture (Mt CO2-equiv/yr)"] <-
+    x[,, "Emi|CO2|Agriculture (Mt CO2/yr)"] +
+    x[,, "Emi|CH4|Agriculture (Mt CH4/yr)"] * 28 +
+    x[,, "Emi|N2O|Agriculture (kt N2O/yr)"] / 1000 * 265
+
+  x <- add_columns(x, "Emi|GHG|Land-Use Change (Mt CO2-equiv/yr)", dim=3.1)
+  x[,, "Emi|GHG|Land-Use Change (Mt CO2-equiv/yr)"] <-
+    x[,, "Emi|CO2|Land-Use Change (Mt CO2/yr)"] +
+    x[,, "Emi|CH4|Land-Use Change (Mt CH4/yr)"] * 28 +
+    x[,, "Emi|N2O|Land-Use Change (kt N2O/yr)"] / 1000 * 265
+
+  x <- add_columns(x, "Emi|GHG|Waste (Mt CO2-equiv/yr)", dim=3.1)
+  x[,, "Emi|GHG|Waste (Mt CO2-equiv/yr)"] <-
+    x[,, "Emi|CO2|Waste (Mt CO2/yr)"] +
+    x[,, "Emi|CH4|Waste (Mt CH4/yr)"] * 28 +
+    x[,, "Emi|N2O|Waste (kt N2O/yr)"] / 1000 * 265
+
+  # GHG total
+  x <- add_columns(x, "Emi|GHGtot (Mt CO2-equiv/yr)", dim=3.1)
+  x[,, "Emi|GHGtot (Mt CO2-equiv/yr)"] <-
+    x[,, "Emi|CO2 (Mt CO2/yr)"] +
+    x[,, "Emi|CH4 (Mt CH4/yr)"] * 28 +
+    x[,, "Emi|N2O (kt N2O/yr)"] / 1000 * 265
+
   return(list(x = x, weight = NULL, 
               unit = c("kt CO2", "kt CH4", "kt N2O"),
               description = "Historical UNFCCC values as REMIND variables"))
