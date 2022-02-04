b41ac4b8cfda24ec998e9942f9ef737adecd07ff
Michaja Pehl <pehl@pik-potsdam.de>
2022-02-04 15:31:18 +0100

    fix readvanRuijven2016() to not double-report USSR and individual SRs

--- R/readvanRuijven2016.R
+++ R/readvanRuijven2016.R
@@ -3,7 +3,8 @@
 #' Read data from van Ruijven et al. 2016,
 #' (http://dx.doi.org/10.1016/j.resconrec.2016.04.016,
 #' https://www.zotero.org/groups/52011/rd3/items/itemKey/6QMNBEHQ), obtained 
-#' through personal communication (e-mail to Michaja Pehl).
+#' through personal communication (e-mail to Michaja Pehl).  Units are tonnes 
+#' per year.
 #'
 #' @md
 #' @return A [`magpie`][magclass::magclass] object.
@@ -14,14 +15,18 @@
 #' @importFrom magrittr %>%
 #' @importFrom quitte add_countrycode_ madrat_mule
 #' @importFrom readxl read_excel
-#' @importFrom tidyr pivot_longer
+#' @importFrom tidyr expand_grid pivot_longer
 #' 
 #' @seealso [`readSource()`]
 
 #' @export
 readvanRuijven2016 <- function() {
-  read_excel(
-    path = './Cement_data_from_Bas.xlsx',
+  USSR_iso3c <- c('ARM', 'AZE', 'BLR', 'EST', 'GEO', 'KAZ', 'KGZ', 'LTU', 
+                  'LVA', 'MDA', 'RUS', 'TJK', 'TKM', 'UKR', 'UZB') 
+  
+  x <- read_excel(
+    # path = './Cement_data_from_Bas.xlsx',
+    path = '~/PIK/swap/inputdata/sources/vanRuijven2016/Cement_data_from_Bas.xlsx',
     sheet = 'Production',
     range = 'A4:AS321') %>% 
     filter(999 != .data$`Region #`) %>% 
@@ -31,7 +36,19 @@ readvanRuijven2016 <- function() {
     add_countrycode_(origin = c('FAO #' = 'fao'), destination = 'iso3c', 
                      warn = FALSE) %>% 
     add_countrycode_(origin = c('FAO Name' = 'country.name'), 
-                     destination = c('iso3c.alt' = 'iso3c'), warn = FALSE) %>% 
+                     destination = c('iso3c.alt' = 'iso3c'), warn = FALSE)
+  
+  # remove Soviet Republics if USSR is available, to avoid double counting
+  x %>% 
+    anti_join(
+      expand_grid(iso3c = USSR_iso3c,
+                  year = x %>% 
+                    filter('USSR' == .data$`FAO Name`) %>% 
+                    pull('year') %>% 
+                    unique()),
+      
+      c('iso3c', 'year')
+    ) %>% 
     mutate(iso3c = ifelse(!is.na(.data$iso3c), .data$iso3c, .data$iso3c.alt),
            # kg * 0.001 t/kg = t
            value = .data$value * 1e-3) %>% 
