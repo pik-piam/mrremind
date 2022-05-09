698dbba8ace871ad784853efef45b048c63db7c3
piontek <piontek@pik-potsdam.de>
2022-05-09 17:37:16 +0200

    add damage parameters for tropical cyclone damage function

---
+++ R/readTCdamageKrichene.R
@@ -0,0 +1,34 @@
+ead TC damage parameters
+#' 
+#' Reads country-specific damage coefficients for tropical cyclones from Krichene et al. (in prep.). Data has been provided by the authors, but will be made publicly available as well.
+#' This contains data for 41 countries (those exposed to tropical cyclones), and two coefficients (constant and linear temperature)
+#' 
+#' @return TC damage coefficients
+#' @author Franziska Piontek
+#' @param subtype data subtype. Either "const" or "tasK"
+
+readTCdamageKrichene <- function(subtype){
+
+	if(subtype == "const"){
+		a <- read.csv(file = "./TC_df_parameters_const.csv") 
+	} else if (subtype == "tasK"){
+		a <- read.csv(file = "./TC_df_parameters_tasK.csv") 
+	}
+	
+	quant <- gsub("estimates_","",colnames(a)[grepl("estimates",colnames(a))])
+	for(s in unique(a$tc_ssp)){
+		for(p in unique(a$persistence)){
+			for(q in quant){
+				a2 <- subset(a,tc_ssp == s & persistence == p)
+				col <- which(colnames(a2) == paste0("estimates_",q))
+				if(s == unique(a$tc_ssp)[1] & p == 0 & q == quant[1]){
+					out <- new.magpie(a2$iso, NULL, paste0(s,".",p,".",q), a2[,col],sets = c("CountryCode","year","SSP.persistence.quantile"))
+				} else {
+					out <- mbind(out,new.magpie(a2$iso, NULL, paste0(s,".",p,".",q), a2[,col],sets = c("CountryCode","year","SSP.persistence.quantile")))
+				}
+			}
+		}
+    }
+	return(out)
+}
+
