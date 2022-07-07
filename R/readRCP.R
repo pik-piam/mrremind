2a3bda011ecd0fdad91f3937f0fdb24b658b701b
Michaja Pehl <pehl@pik-potsdam.de>
2022-07-07 09:52:55 +0200

    fix pivot_longer column selection in readRCP()

--- R/readRCP.R
+++ R/readRCP.R
@@ -21,43 +21,30 @@ readRCP <- function(subtype) {
 
   dat <- as.data.frame(do.call(rbind, lapply(list.files(pattern = ".xls$"), read_excel)))
 
-  if (subtype=="Waste") {
+  if (subtype == "Waste") {
     dat <- dat %>%
-      filter(.data$Region %in% c("R5ASIA", "R5LAM", "R5MAF", "R5OECD", "R5REF")) %>%
-      filter(grepl("Waste", .data$Variable)) %>%
-      pivot_longer(cols = all_of(setdiff(colnames(.data),
-                                         c("Region", "Scenario", "Variable",
-                                           "Unit"))),
-                   names_to = 'year') %>%
-      mutate(year = as.numeric(.data$year)) %>%
-      separate("Variable", c("type", "source"), sep = " - " ) %>%
-      rename(region = 'Region', scenario = 'Scenario') %>%
-      select("region", "year", "scenario", "type","source", "value")
+      filter(.data$Region %in% c("R5ASIA", "R5LAM", "R5MAF", "R5OECD", "R5REF"),
+             grepl("Waste", .data$Variable)) %>%
+      pivot_longer(cols = matches('^[0-9]*$'), names_to = 'year',
+                   names_transform = list(year = as.numeric)) %>%
+      separate("Variable", c("type", "source"), sep = " - ") %>%
+      select(region = 'Region', 'year', scenario = 'Scenario', 'type', 'source',
+             'value') %>%
+      mutate(region = sub('^R5', '', .data$region))
 
-    dat$region <- substr(dat$region, 3, 7)
-    dat$value  <- as.numeric(dat$value)
-
-  } else if (subtype=="AviationShipping") {
+  } else if (subtype == "AviationShipping") {
     dat <- dat %>%
       filter(.data$Region == "World",
              grepl("Aviation|Shipping", .data$Variable)) %>%
-      pivot_longer(cols = all_of(setdiff(colnames(.data),
-                                         c("Region", "Scenario", "Variable",
-                                           "Unit"))),
-                   names_to = 'year') %>%
-      mutate(year = as.numeric(.data$year),
-             Region = "GLO") %>%
-      separate("Variable", c("type", "source"), sep = " - " ) %>%
-      rename(region = 'Region', scenario = 'Scenario') %>%
-      select("region", "year", "scenario", "type", "source","value")
-
-      dat$value  <- as.numeric(dat$value)
-
+      pivot_longer(cols = matches('^[0-9]*$'), names_to = 'year',
+                   names_transform = list(year = as.numeric)) %>%
+      mutate(Region = "GLO") %>%
+      separate("Variable", c("type", "source"), sep = " - ") %>%
+      select(region = 'Region', 'year', scenario = 'Scenario', 'type', 'source',
+             'value')
   } else {
     stop("Invalid subtype. Must be 'Waste' or 'AviationShipping'")
   }
-
-
 
   dat$scenario <- recode(dat$scenario,
                          `IMAGE - RCP3-PD (2.6)`="rcp26",
