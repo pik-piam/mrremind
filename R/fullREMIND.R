61f75848ba3ce33df9c040dc04b54427ae1036d7
Falk Benke <falk.benke@pik-potsdam.de>
2022-08-23 11:04:35 +0200

    Calculate emission factors for feedstocks in the chemicals industry

--- R/fullREMIND.R
+++ R/fullREMIND.R
@@ -94,6 +94,7 @@ fullREMIND <- function(rev = 0) {
   calcOutput("GAINSEmi",       subtype = "emission_factors",          round = 5, file = "ef_gains.cs4r")
   calcOutput("GAINSEmi",       subtype = "emissions_starting_values", round = 5, file = "f11_emiAPexsolve.cs4r")
   calcOutput("EmissionFactors", subtype = "emission_factors",          round = 5, file = "f11_emiFacAP.cs4r")
+  calcOutput("EmissionFactorsFeedstocks", round = 5, file = "???.cs4r") # TODO
   calcOutput("EmiLULUCFCountryAcc", subtype = "UNFCCC",          round = 5, file = "p_EmiLULUCFCountryAcc.cs4r")
 
   #-------------- energy/technology parameters ---------------------------------------------------------
