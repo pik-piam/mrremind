a18b7c6090fb1e0fa74491873290263feba16d80
Pascal Weigmann <pascalwe@LT944.pik-potsdam.de>
2022-10-05 17:08:25 +0200

    readEmber wip

---
+++ R/readEmber.R
@@ -0,0 +1,30 @@
+#' Read Ember Yearly Electricity Data
+#'
+#' @return A [`magpie`][magclass::magclass] object.
+#'
+#' @author Pascal Weigmann
+#'
+#' @seealso [`readSource()`]
+#' @source https://ember-climate.org/data-catalogue/yearly-electricity-data/
+#'
+#' @importFrom dplyr select
+#' @importFrom magclass as.magpie
+#'
+#' @export
+
+readEmber <- function() {
+  filename <- "yearly_full_release_long_format.csv"
+  df <- read.csv(filename)
+
+  # filter out regions by only choosing rows that don't have a blank country.code
+  df <- df[df$Country.code != "", ]
+
+  # combine category columns to one variable column
+  df$variable <- paste(df$Category, df$Subcategory, df$Variable, sep = "|")
+  df <- select(df, c("Country.code", "Year", "variable", "Unit", "Value"))
+
+  # convert to magpie object
+  x <- as.magpie(df, spatial="Country.code", temporal="Year", tidy = TRUE)
+
+  return(x)
+}
