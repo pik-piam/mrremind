c49313d20fd3400fe3d7f9c30b36b78d990bd421
Falk Benke <falk.benke@pik-potsdam.de>
2022-12-22 16:46:02 +0100

    increment version to dev

--- R/readIEA_WEO_2021.R
+++ R/readIEA_WEO_2021.R
@@ -24,18 +24,18 @@ readIEA_WEO_2021 <- function() { # nolint
     ) %>% rename_all(tolower)
   ) %>%
     mutate(
+      variable = paste0(!!sym("category"), "-", !!sym("product"), "-", !!sym("flow"), " (", !!sym("unit"), ")"),
       year = as.numeric(!!sym("year")),
       value = as.numeric(!!sym("value"))
     ) %>%
-    select("region", "year", "scenario", "category", "product", "flow", "unit", "value") %>%
+    select("region", "year", "scenario", "variable", "value") %>%
     group_by(
-      !!sym("region"), !!sym("year"), !!sym("scenario"), !!sym("category"),
-      !!sym("product"), !!sym("flow"), !!sym("unit")
+      !!sym("region"), !!sym("year"), !!sym("scenario"), !!sym("variable")
     ) %>%
     distinct() %>%
     ungroup()
 
-  as.magpie(data, temporal = 2, spatial = 1, datacol = 8) %>%
+  as.magpie(data, temporal = 2, spatial = 1, datacol = 5) %>%
     magpiesort() %>%
     return()
 }
