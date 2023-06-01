5994e2cb86aaf1a79a1c0f898e9c29fe4d1b192f
Falk Benke <falk.benke@pik-potsdam.de>
2023-06-01 14:26:19 +0200

    filter unprecise data in historical.mif for IEA WEO 2021

--- R/fullVALIDATIONREMIND.R
+++ R/fullVALIDATIONREMIND.R
@@ -51,22 +51,29 @@ fullVALIDATIONREMIND <- function(rev = 0) {
     append = TRUE, warnNA = FALSE, try = TRUE, isValidation = TRUE
   )
 
-  calcOutput(
-    type = "IEA_WEO_2021", subtype = "global", aggregate = columnsForAggregation,
-    file = valfile,append = TRUE, warnNA = FALSE, try = TRUE, isValidation = TRUE
-  )
-
-  calcOutput(
-    type = "IEA_WEO_2021", subtype = "region", aggregate = columnsForAggregation,
-    file = valfile, append = TRUE, warnNA = FALSE, try = TRUE, isValidation = TRUE
-  )
-
   ## industry value added ----
   calcOutput(
     type = "UNIDO", subtype = "INDSTAT2", file = valfile,
     aggregate = columnsForAggregation, append = TRUE, warnNA = FALSE,
     try = TRUE
   )
+
+  ## add WEO data on regional and global level ----
+  weo <- calcOutput(
+    type = "IEA_WEO_2021", subtype = "global", aggregate = columnsForAggregation,
+    warnNA = FALSE, try = TRUE, isValidation = TRUE
+  )
+
+  weo <- weo["GLO", , ]
+  write.report(weo, file = file.path(getConfig("outputfolder"), valfile), append = TRUE)
+
+  weo <- calcOutput(
+    type = "IEA_WEO_2021", subtype = "region", aggregate = columnsForAggregation,
+    warnNA = FALSE, try = TRUE, isValidation = TRUE
+  )
+
+  weo <- weo["GLO", , invert = T]
+  write.report(weo, file = file.path(getConfig("outputfolder"), valfile), append = TRUE)
 
   # filter variables that are too imprecise on regional level ----
   filter_historical_mif()
