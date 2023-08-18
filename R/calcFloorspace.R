69b361c21266d6c8ebd4dd6b06634575c706af45
robinhasse <robin.krekeler@pik-potsdam.de>
2023-08-18 13:17:59 +0200

    Subsectoral floor space

--- R/calcFloorspace.R
+++ R/calcFloorspace.R
@@ -1,8 +1,26 @@
+#' Floor space in buildings
+#'
+#' Residential, commercial and total floor space from EDGE-B. Set
+#'
+#' @author Antoine Levesque, Robin Hasse
+#'
+#' @param onlyTotal boolean, only give total instead of sub-sectoral floor space
+#' @return MAgPIE object with buildings floor space
+#'
+#' @importFrom madrat readSource
+#' @importFrom magclass collapseNames
+#' @export
+#'
+calcFloorspace <- function(onlyTotal = FALSE) {
 
-calcFloorspace <- function() {
-  
-  data <- readSource("EDGE", subtype = "Floorspace") 
-  data <- collapseNames(data[,,"buildings"])
-  
-  return(list(x=data,weight=NULL, unit = "million m2", description = "Buildings floorspace"))
+  data <- readSource("EDGE", subtype = "Floorspace")
+
+  if (onlyTotal) {
+    data <- collapseNames(data[, , "buildings"])
+  }
+
+  return(list(x = data,
+              weight = NULL,
+              unit = "million m2",
+              description = "Buildings floor space"))
 }
