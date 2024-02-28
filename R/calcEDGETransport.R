0629bb3fed56efdd3947f3069f024fa9694ea7df
johannah-pik <89136160+johannah-pik@users.noreply.github.com>
2024-02-28 16:35:00 +0100

    refactor transport mrremind functions

--- R/calcEDGETransport.R
+++ R/calcEDGETransport.R
@@ -10,120 +10,13 @@
 #' }
 #'
 #'@importFrom data.table as.data.table
-calcEDGETransport <- function(subtype = "logit_exponent") {
-  value <- i.value <- NULL
-
-  if (subtype %in% c("logit_exponent", "ptab4W")) {
-      conv = FALSE
-   } else {
-      conv = TRUE
-   }
-
-  weightInt <- calcOutput("GDP", aggregate = F)
-  get_weight <- function(data, weightInt) {
-    year_inter = getYears(data)
-    ## define weight for intensive entries (for weighted average)
-    weightInt <- time_interpolate(
-      weightInt,
-      year_inter,
-      extrapolation_type="constant")[,, getNames(data, dim=1)]
-    ## create an empty object that has the same dimensions as data
-    weight <- new.magpie(cells_and_regions = getRegions(data), years = getYears(data), names = getNames(data), fill = 0)
-    ## use the GPD for each SSP in data to fill up the empty weight (it needs as many repetitions as SSP* is called in data)
-    regions <- getRegions(weightInt)
-    years <- getYears(weightInt)
-    for (gdpscen in getNames(weightInt)) {
-      mselect(weight, region=regions, year=years, data=gdpscen) <- weightInt[regions, years, gdpscen]
-    }
-    getSets(weight, fulldim=F)[3] <- getSets(data, fulldim=F)[3]
-    return(weight)
-  }
-
-  if(subtype == "pm_trp_demand"){
-    fe_dem <- as.data.table(as.quitte(readSource("EDGETransport", "fe_demand_tech")))[
-    , c("model", "scenario", "variable", "unit") := NULL
-    ]
-    fe2es  <- as.data.table(as.quitte(readSource("EDGETransport", "fe2es")))[
-    , c("model", "scenario", "variable", "unit") := NULL
-    ]
-    es_dem <- fe_dem[fe2es, on=c("period", "region", "GDP_scenario", "DEM_scenario", "EDGE_scenario", "all_teEs")][
-    , sum(value * i.value), by=c("period", "region", "GDP_scenario", "DEM_scenario", "EDGE_scenario", "all_in")
-    ]
-    data <- as.magpie(es_dem, spatial=2, temporal=1, datacol=7)
-  }else{
-    data <- readSource("EDGETransport", subtype, convert = conv)
-  }
+calcEDGETransport <- function(subtype) {
 
   switch(subtype,
-         "logit_exponent" = {
+         ".." = {
            weight = NULL
            unit = "[-]"
-           description = "Logit exponent values for transport alternatives"
-         },
-         "harmonized_intensities" = {
-           weight = get_weight(data, weightInt)
-           unit = "Passenger transport: [EJ/Mpkm]; freight transport: [EJ/Mtkm]"
-           description = "Energy intensity for transport modes and fuel, harmonized on IEA balances"
-         },
-         "value_time" = {
-           weight = get_weight(data, weightInt)
-           unit = "Passenger transport: [2005US$/pkm]; freight transport: [2005US$/tkm]"
-           description = "Value of time for passenger transport modes"
-         },
-         "pref" = {
-           weight = get_weight(data, weightInt)
-           unit = "LDVs 4wheelers: inconvenience cost [2005US$/pkm]; all other modes: [-] share weight, a dimensionless parameter reflecting consumer preferences"
-           description = "Inconvenience cost reflecting availability of infrastructure"
-         },
-         "ptab4W" = {
-           weight = NULL
-           unit = "[-]"
-           description = "LDVs 4wheelers: factors for the inconvenience costs."
-         },
-         "price_nonmot" = {
-           weight = get_weight(data, weightInt)
-           unit = "Passenger transport: [2005US$/pkm]; freight transport: [2005US$/tkm]"
-           description = "Price for non motorized transport modes (Walking; Cycling)"
-         },
-         "UCD_NEC_iso" = {
-           weight = get_weight(data, weightInt)
-           unit = "Passenger transport: [2005US$/pkm]; freight transport: [2005US$/tkm]"
-           description = "Non energy costs for all motorized transport modes, both total and purchase for LDVs"
-         },
-         "loadFactor" = {
-           weight = get_weight(data, weightInt)
-           unit = "Passenger transport: [pass/veh]; freight transport: [ton/veh]"
-           description = "Load factor for all motorized transport modes"
-         },
-         "annual_mileage" = {
-           weight = get_weight(data, weightInt)
-           unit = "km/veh/year"
-           description = "Annual mileage for selected transport modes"
-         },
-         "esCapCost" = {
-           weight = get_weight(data, weightInt)
-           unit = "Passenger transport [2005US$/pkm]; freight transport: [2005US$/tkm]"
-           description = "Transport ES demand trajectories for the CES tree."
-         },
-         "fe2es" = {
-           weight = get_weight(data, weightInt)
-           unit = "Passenger transport [trn pkm/Twa], freight transport [trn tkm/Twa]"
-           description = "Energy efficiency of CES level nodes for Transport."
-         },
-         "pm_trp_demand" = {
-           weight = NULL
-           unit = "Passenger transport [trn pkm], freight transport [trn tkm]"
-           description = "CES level transport demand."
-         },
-         "fe_demand_tech" = {
-           weight = NULL
-           unit = "TWa"
-           description = "FE demand divided by technologies for different ES on the CES level."
-         },
-         "pm_fe_demand_EDGETbased" = {
-           weight = NULL
-           unit = "EJ"
-           description = "UE demand divided by technologies for different ES on the CES level."
+           description = ".."
          })
 
   return(list(x           = data,
--- R/convertEDGETransport.R
+++ R/convertEDGETransport.R
@@ -4,57 +4,21 @@
 #'
 #' @param subtype EDGE entries
 #' @param x MAgPIE object containing EDGE values at ISO country resolution
-#' @return EDGETransport data as MAgPIE object aggregated to ISO level
+#' @return EDGETransport data as MAgPIE object disaggregated to ISO level
 #' @author Marianna Rottoli
 #'
 #' @importFrom data.table setDT
 #'
 convertEDGETransport = function(x, subtype) {
-  `.` <- CountryCode <- RegionCode <- NULL
-  if (subtype %in% c("esCapCost", "fe_demand_tech", "fe2es", "UCD_NEC_iso", "harmonized_intensities", "value_time", "pref", "loadFactor", "annual_mileage", "f35_bunkers_fe")) {
-    ## magpie object creates NA whenever the initial dt is not symmetric (entry absent in ISO1 but exists in ISO2)
-    ## the NAs are therefore converted to 0
-    x[is.na(x)] <- 0
-  }
-  ## load mapping
-  mappingfile <- setDT(toolGetMapping("regionmapping_21_EU11.csv",type="regional", 
-                      where = "mappingfolder"))[, .(iso = CountryCode, region = RegionCode)]
-  ## for intensive values, the weight is NULL
-  if (subtype %in% c("fe2es", "UCD_NEC_iso", "harmonized_intensities", "value_time", "pref", "loadFactor", "annual_mileage", "shares_LDV_transport", "price_nonmot", "esCapCost")) {
-    x = toolAggregate(x = x, rel = mappingfile, weight = NULL, from = "region", to = "iso")
-  }
 
-  ## for extensive values and the edge_esm module, the weight is ES
-  if (subtype == "fe_demand_tech") {
-    wgt = readSource("EDGETransport", subtype ="demISO")
-    ## rename the columns of the weight
-    x = toolAggregate(x = x, weight = wgt, rel = mappingfile, from = "region", to = "iso")
-  }
-
-  ## for extensive values and the complex module, the weight is GDP
-  if (subtype == "pm_fe_demand_EDGETbased") {
-    gdp <- calcOutput("GDP", aggregate = F)[,,"gdp_SSP2"]
-    ## interpolate missing time steps
-    gdp <- time_interpolate(gdp, getYears(x))
-
-    ## rename the columns of the weight
-    getSets(gdp) <- c("iso", "year", "variable")
+  mappingfile <- setDT(toolGetMapping("regionmapping_21_EU11.csv", type = "regional",
+                                      where = "mappingfolder"))[, .(iso = CountryCode, region = RegionCode)]
+  if subtype %in% c() {
+    gdp <- calcOutput("GDP", aggregate = F)
     x = toolAggregate(x = x, weight = gdp, rel = mappingfile, from = "region", to = "iso")
+  } else {
+    result <- toolAggregate(x = x, rel = mappingfile, weight = NULL, from = "region", to = "iso")
   }
 
-
-  if (subtype %in% c("shares_LDV_transport")) {
-    ## only the first EDGE-T scenario for SSP2 is used as a proxy for the LDV shares
-    x <- x[,, "gdp_SSP2.Mix1.share_LDV_totliq.shares_LDV_transport"]
-
-    for (year in getYears(x, as.integer = T)){
-      x[,year,] <- as.vector(x[,c(2010),]) + ((0.55 - as.vector(x[,c(2010),]))/(2100-2010))*(year-2010)
-    }
-    #extending values
-    x <- time_interpolate(x, integrate_interpolated_years=T, interpolated_year = seq(from = 1990, to = 2100), extrapolation_type = "linear")
-    x <- time_interpolate(x, integrate_interpolated_years=T, interpolated_year = c(seq(from = 1970, to = 1989),seq(from = 2101, to = 2150)), extrapolation_type = "constant")
-    }
-
-  result = x
   return(result)
 }
