bcce3b6e27573ab0ecce7afae4cecc60c6cea493
Michaja Pehl <pehl@pik-potsdam.de>
2024-02-29 15:32:01 +0100

    extend GlobalCCSinstitute source to 2023 update

--- R/convertGlobalCCSinstitute.R
+++ R/convertGlobalCCSinstitute.R
@@ -1,15 +1,45 @@
+#' Convert Global CCS Institute Project Database
+#'
+#' @md
+#' @param x A [`magpie`][magclass::magclass] object returned by
+#'     [readGlobalCCSinstitute()].
+#' @inherit readGlobalCCSinstitute
+#'
+#' @return A [`magpie`][magclass::magclass] object.
+#'
+#' @importFrom dplyr %>%
+#' @importFrom madrat toolCountry2isocode toolCountryFill
+#' @importFrom magclass dimSums getRegions getItems<-
+#' @importFrom quitte add_countrycode_ madrat_mule
+#'
+#' @export
+convertGlobalCCSinstitute <- function(x, version = '08-09-2017') {
+  if ('08-09-2017' == version) { # 08-09-2017 ----
 
-
-convertGlobalCCSinstitute <- function(x) {
- 
     # sum over districts of a country
-    y <- dimSums(x,dim=3)
-    
+    y <- dimSums(x, dim = 3)
+
     # transfer into ISO country names
-    getRegions(y) <- toolCountry2isocode(getRegions(y))
-    
+    getItems(y, dim = 1) <- toolCountry2isocode(getRegions(y))
+
     # fill all other countries with 0
-    y <- toolCountryFill(y, fill = 0, verbosity=2)
-  
-  return(y)
+    y <- toolCountryFill(y, fill = 0, verbosity = 2)
+
+    return(y)
+  }
+  else if ('2023-11' == version) { # 2023-11 ----
+    x %>%
+      madrat_mule() %>%
+      add_countrycode_(origin = c('Country' = 'country.name'),
+                          destination = 'iso3c') %>%
+         select(-'Lifecycle stage', 'Facility', 'iso3c', 'Operational date',
+                'Facility Industry',
+                'Capture, transport and/or storage capacity (Mtpa CO2)',
+                'Facility storage code') %>%
+      madrat_mule() %>%
+      return()
+  }
+  else {
+    stop('Unsupported version argument.')
+  }
 }
