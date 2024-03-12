710115d57d0418b95017d618402e7ec8327631c4
Falk Benke <falk.benke@pik-potsdam.de>
2024-03-12 17:18:20 +0100

    remove warnings from readBGR

--- R/convertBGR.R
+++ R/convertBGR.R
@@ -1,21 +1,19 @@
 #' Converts BGR oil, gas, coal and uranium reserves data
-#' 
+#'
 #' @param x MAgPIE object to be converted
 #' @param subtype data subtype. Either "oil", "gas", "coal" or "uranium".
-#' @return A MAgPIE object containing BGR (Federal Institute for Geosciences and Natural Resources) country reserves disaggregated data of oil, gas, coal and uranium.
+#' @return A MAgPIE object containing BGR (Federal Institute for Geosciences and
+#' Natural Resources) country reserves disaggregated data of oil, gas, coal and uranium.
 #' @author Renato Rodrigues
 #' @examples
-#' 
-#' \dontrun{ a <- convertBGR(x,subtype="oil")
+#' \dontrun{
+#' a <- convertBGR(x, subtype = "oil")
 #' }
-#'  
-
- convertBGR <- function(x,subtype) {
-   # rename countries to REMIND iso codes
-   getRegions(x) <- toolCountry2isocode(getRegions(x))
-   x[is.na(x)] <- 0 # setting NA values to zero
-   x  <- toolCountryFill(x,fill=0,verbosity=2) # fill countries with no data
-   return(x)
- }
- 
- 
+#'
+convertBGR <- function(x, subtype) {
+  # rename countries to REMIND iso codes
+  getItems(x, dim = 1) <- toolCountry2isocode(getItems(x, dim = 1))
+  x[is.na(x)] <- 0 # setting NA values to zero
+  x <- toolCountryFill(x, fill = 0, verbosity = 2) # fill countries with no data
+  return(x)
+}
--- R/readBGR.R
+++ R/readBGR.R
@@ -1,34 +1,34 @@
 #' Read BGR oil, gas, coal and uranium reserves data
-#' 
+#'
 #' Read-in BGR csv files as magclass object
-#' 
-#' 
+#'
+#'
 #' @param subtype data subtype. Either "oil", "gas", "coal" or "uranium".
-#' @return magpie object of the BGR (Federal Institute for Geosciences and Natural Resources) data of reserves of oil, gas, coal and uranium per country.
+#' @return magpie object of the BGR (Federal Institute for Geosciences and
+#' Natural Resources) data of reserves of oil, gas, coal and uranium per country.
 #' @author Renato Rodrigues
 #' @seealso \code{\link{readSource}}
 #' @examples
-#' 
-#' \dontrun{ a <- readSource(type="BGR",subtype="oil")
+#' \dontrun{
+#' a <- readSource(type = "BGR", subtype = "oil")
 #' }
-#'  
-
- readBGR <- function(subtype) {
-   if (subtype == "oil") {
-     #Reading oil resources per country values in 2009 [Mt] from csv
-     data <- read.csv("oil_reserves.csv",sep=";")
-   } else if (subtype== "gas") {
-     #Reading gas resources per country values in 2009 [Mrd. m³] from csv
-     data <- read.csv("gas_reserves.csv",sep=";")
-   } else if (subtype== "coal") {
-     #Reading coal resources per country values in 2009 [Mt] from csv
-     data <- read.csv("coal_reserves.csv",sep=";")
-   } else if (subtype== "uranium") {
-     #Reading uranium resources per country values in 2009 [kt U] from csv
-     data <- read.csv("uranium_reserves.csv",sep=";")
-   } else {
-     stop("Not a valid subtype!")
-   }
-   x <- as.magpie(data,temporal=0,spatial=1,datacol=2)
-   return(x)
- }  
+#'
+readBGR <- function(subtype) {
+  if (subtype == "oil") {
+    # Reading oil resources per country values in 2009 [Mt] from csv
+    data <- read.csv("oil_reserves.csv", sep = ";")
+  } else if (subtype == "gas") {
+    # Reading gas resources per country values in 2009 [Mrd. m³] from csv
+    data <- read.csv("gas_reserves.csv", sep = ";")
+  } else if (subtype == "coal") {
+    # Reading coal resources per country values in 2009 [Mt] from csv
+    data <- read.csv("coal_reserves.csv", sep = ";")
+  } else if (subtype == "uranium") {
+    # Reading uranium resources per country values in 2009 [kt U] from csv
+    data <- read.csv("uranium_reserves.csv", sep = ";")
+  } else {
+    stop("Not a valid subtype!")
+  }
+  x <- as.magpie(data, temporal = 0, spatial = 1, datacol = 2)
+  return(x)
+}
