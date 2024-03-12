1bc8889b7de2abe7942100cac5bc0f2f3830102b
Falk Benke <falk.benke@pik-potsdam.de>
2024-03-12 16:26:46 +0100

    remove warnings in readNREL

--- R/convertNREL.R
+++ R/convertNREL.R
@@ -7,25 +7,26 @@
 #' @return NRELWirsenius data as MAgPIE object aggregated to country level
 #' @author Lavinia Baumstark
 #' @examples
-#'
-#' \dontrun{ a <- convertNREL(x,subtype="onshore")
+#' \dontrun{
+#' a <- convertNREL(x, subtype = "onshore")
 #' }
 #' @importFrom madrat toolCountryFill
 convertNREL <- function(x) {
-
   # rename countries with ISO-code
-  getRegions(x) <- toolCountry2isocode(getRegions(x))
+  getItems(x, dim = 1) <- toolCountry2isocode(getItems(x, dim = 1))
 
   #-------------------- allocation of aggregations -------------------------------
   # "ANT" -> "SXM", "CUW", "BES"
   # "YUG" -> "SRB", "MNE"  # YUG seems to be only these two, "SVN", "HRV", "MKD", "BIH"
-  m <- matrix(c(c("ANT","ANT","ANT",       "YUG","YUG"), #,"YUG","YUG","YUG","YUG"),
-                c("SXM","CUW","BES",       "SRB","MNE")),5) #,"SVN","HRV","MKD","BIH")),9)
-  w <- calcOutput("Population",aggregate=FALSE)[m[,2],2010,"pop_SSP2"]
-  x_split <- toolAggregate(x[c("ANT","YUG"),,],m,weight=w)
+  m <- matrix(c(
+    c("ANT", "ANT", "ANT", "YUG", "YUG"), # ,"YUG","YUG","YUG","YUG"),
+    c("SXM", "CUW", "BES", "SRB", "MNE")
+  ), 5) # ,"SVN","HRV","MKD","BIH")),9)
+  w <- calcOutput("Population", aggregate = FALSE)[m[, 2], 2010, "pop_SSP2"]
+  x_split <- toolAggregate(x[c("ANT", "YUG"), , ], m, weight = w)
   # delete ANT and YUG from x
-  x <- x[c("ANT","YUG"),,invert=TRUE]
-  x <- mbind(x,x_split)
+  x <- x[c("ANT", "YUG"), , invert = TRUE]
+  x <- mbind(x, x_split)
 
   #--------------------------------------------------------------------------------
 
--- R/readNREL.R
+++ R/readNREL.R
@@ -1,55 +1,55 @@
 #' Read NREL
-#' 
+#'
 #' Read-in NREL xlsx file as magclass object
-#' 
-#' 
+#'
+#'
 #' @param subtype type either "onshore" or "offshore"
 #' @return magpie object of NREL
 #' @author Lavinia Baumstark
 #' @seealso \code{\link{readSource}}
 #' @examples
-#' 
-#' \dontrun{ a <- readSource(type="NREL",subtype="onshore")
+#' \dontrun{
+#' a <- readSource(type = "NREL", subtype = "onshore")
 #' }
-#' 
+#'
 readNREL <- function(subtype) {
 
-    if (subtype == "onshore") {
+  if (subtype == "onshore") {
+
     # read in data
-    x <- as.data.frame(read_excel("nrelcfddawindsc20130603.xlsx", sheet="Onshore Energy",skip=2))
-    x <- x[1:182,1:32]
+    x <- as.data.frame(read_excel("nrelcfddawindsc20130603.xlsx", sheet = "Onshore Energy", skip = 2))
+    x <- x[1:182, 1:32]
     x <- x[-length(names(x))]
-    names(x) <- sub("\\.[0-9]*$","",names(x))
-    names(x) <- gsub(" ",".",names(x))
-    colnames(x)[2:11]  <- paste("near",colnames(x)[2:11],sep=".")
-    colnames(x)[12:21] <- paste("transitional ",colnames(x)[12:21],sep=".")
-    colnames(x)[22:31] <- paste("far",colnames(x)[22:31],sep=".")
-    
+    names(x) <- sub("\\.[0-9]*$", "", names(x))
+    names(x) <- gsub(" ", ".", names(x))
+    colnames(x)[2:11] <- paste("near", colnames(x)[2:11], sep = ".")
+    colnames(x)[12:21] <- paste("transitional ", colnames(x)[12:21], sep = ".")
+    colnames(x)[22:31] <- paste("far", colnames(x)[22:31], sep = ".")
+
   } else if (subtype == "offshore") {
-  
+
     # read in data
-    x <- as.data.frame(read_excel("nrelcfddawindsc20130603.xlsx", sheet="Offshore Energy",skip=3))
-    x <- x[1:560,1:33]
+    x <- as.data.frame(read_excel("nrelcfddawindsc20130603.xlsx", sheet = "Offshore Energy", skip = 3))
+    x <- x[1:560, 1:33]
     x <- x[-length(names(x))]
-    names(x) <- sub("\\.[0-9]*$","",names(x))
-    names(x) <- gsub(" ",".",names(x))
-    colnames(x)[3:12]  <- paste("near",colnames(x)[3:12],sep=".")
-    colnames(x)[13:22] <- paste("transitional ",colnames(x)[13:22],sep=".")
-    colnames(x)[23:32] <- paste("far",colnames(x)[23:32],sep=".")
+    names(x) <- sub("\\.[0-9]*$", "", names(x))
+    names(x) <- gsub(" ", ".", names(x))
+    colnames(x)[3:12] <- paste("near", colnames(x)[3:12], sep = ".")
+    colnames(x)[13:22] <- paste("transitional ", colnames(x)[13:22], sep = ".")
+    colnames(x)[23:32] <- paste("far", colnames(x)[23:32], sep = ".")
     x$depth_class[is.na(x$depth_class)] <- "Total"
-    x$IAM.country <- gsub(" Total","",x$IAM.country)
-    x$IAM.country[is.na(x$IAM.country)] <- x$IAM.country[which(is.na(x$IAM.country))-1]
-    x$IAM.country[is.na(x$IAM.country)] <- x$IAM.country[which(is.na(x$IAM.country))-1]
-  
-    
-    }
-  
+    x$IAM.country <- gsub(" Total", "", x$IAM.country)
+    x$IAM.country[is.na(x$IAM.country)] <- x$IAM.country[which(is.na(x$IAM.country)) - 1]
+    x$IAM.country[is.na(x$IAM.country)] <- x$IAM.country[which(is.na(x$IAM.country)) - 1]
+  }
+
   # turn into a magclass object
-  x <- as.magpie(x, spatial=1) 
-  
+  x <- as.magpie(x, spatial = 1)
+
   if (subtype == "offshore") {
-    x <- x["Guadeloupe and Martinique",,,invert=TRUE]    # FIXME find better solution how to deal with this part of ANT
-  }  
+    # FIXME find better solution how to deal with this part of ANT
+    x <- x["Guadeloupe and Martinique", , , invert = TRUE]
+  }
 
   return(x)
 }
