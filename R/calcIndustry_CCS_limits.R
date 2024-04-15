2e98683f07c03f24c76d06f7daf063b32b6c9eb4
Michaja Pehl <pehl@pik-potsdam.de>
2024-04-15 10:45:51 +0200

    limit CCS endowment by subsector emissions

--- R/calcIndustry_CCS_limits.R
+++ R/calcIndustry_CCS_limits.R
@@ -28,7 +28,7 @@
 #' The resulting project capacities constitute the limits on industry subsector
 #' CCS capacity for 2025.  The limit on CCS capacities for regions (or countries
 #' if `region_mapping` is `NULL`) is set to a value of total 2025 subsector CCS
-#' capacity, times the regions share in 2025 subsector activity (e.g. cement
+#' capacity, times the regions share in subsector activity (e.g. cement
 #' production) of the SSP2EU scenario
 #' - in 2030 if the region as some CCS capacity in 2025 in a different industry
 #'   subsector, or
@@ -41,6 +41,8 @@
 #' @param a1,a2 Annual growth factors of CCS capacity limits, for the first ten
 #'     years and thereafter, default to `0.7` and `0.2` (70 % and 20 %,
 #'     respectively).
+#' @param installation_minimum Minimum emission capacity (in MtCO~2~/year)
+#'     capacities are rounded up to.  Defaults to `0.5` (500 ktCO~2~/year).
 #' @param stage_weight  A named vector of weight factors for different lifecycle
 #'     stages.  See Details.
 #' @param facility_subsector A named vector mapping the "Facility Industry" of
@@ -64,7 +66,7 @@
 #' @export
 calcIndustry_CCS_limits <- function(
     a1 = 0.5, a2 = 0.2,
-    installation_minimum = 0,
+    installation_minimum = 0.5,
     stage_weight = c('Operational'          = 1,
                      'In construction'      = 1,
                      'Advanced development' = 0.5,
@@ -82,15 +84,17 @@ calcIndustry_CCS_limits <- function(
   facility_subsector <- tibble(`Facility Industry` = names(facility_subsector),
                                subsector = facility_subsector)
 
-  ## read 2025 SSP2EU industry activity ----
-  ind_activity_2025 <- calcOutput('FEdemand', aggregate = FALSE,
-                                  years = 2025) %>%
+  remind_timesteps <- unique(quitte::remind_timesteps$period)
+
+  ## read SSP2EU industry activity ----
+  ind_activity <- calcOutput('FEdemand', aggregate = FALSE,
+                             years = remind_timesteps) %>%
     `[`(,,paste0('gdp_SSP2EU.',
                  c('ue_cement', 'ue_chemicals', 'ue_steel_primary'))) %>%
     magclass_to_tibble() %>%
     mutate(subsector = sub('ue_([^_]+).*', '\\1', .data$item),
            .keep = 'unused') %>%
-    select(iso3c = 'region', 'subsector', activity = 'value')
+    select(iso3c = 'region', 'subsector', period= 'year',  activity = 'value')
 
   ## set/check region mapping ----
   iso3c_list <- read_delim(
@@ -134,11 +138,12 @@ calcIndustry_CCS_limits <- function(
     complete(crossing(!!!syms(c('region', 'subsector', 'period'))),
              fill = list(value = 0)) %>%
     full_join(
-      full_join(ind_activity_2025, region_mapping, 'iso3c') %>%
-        group_by(.data$region, .data$subsector) %>%
+      ind_activity %>%
+        full_join(region_mapping, 'iso3c') %>%
+        group_by(.data$region, .data$subsector, .data$period) %>%
         summarise(activity = sum(.data$activity), .groups = 'drop'),
 
-      c('region', 'subsector')
+      c('region', 'subsector', 'period')
     ) %>%
     replace_na(list(period = 2025L, value = 0)) %>%
     # classes: - A: non-zero 2025 data (is continued)
@@ -155,8 +160,8 @@ calcIndustry_CCS_limits <- function(
         0 != sum(.data$value[2025 == .data$period]) ~ 'B',
         0 == sum(.data$value[2025 == .data$period]) ~ 'C')) %>%
     ungroup() %>%
-    complete(nesting(!!!syms(c('region', 'subsector', 'class', 'activity'))),
-             period = unique(quitte::remind_timesteps$period),
+    complete(nesting(!!!syms(c('region', 'subsector', 'class'))),
+             period = remind_timesteps,
              fill = list(value = NA)) %>%
     group_by(.data$period, .data$subsector) %>%
     mutate(activity.total = sum(.data$activity, na.rm = TRUE),
@@ -165,8 +170,21 @@ calcIndustry_CCS_limits <- function(
     group_by(.data$subsector) %>%
     fill('value.total', .direction = 'down') %>%
     arrange(.data$region, .data$subsector, .data$period) %>%
+    # conversion factors for activity to emissions for cement and steel
+    full_join(
+      tribble(
+        ~subsector,    ~factor,   # ad-hoc emission factors
+        'cement',      0.78,      # Gt cement/a -> GtCO2/a
+        'chemicals',   NA,
+        'steel',       1.85),     # Gt steel/a  -> GtCO2/a
+
+      'subsector') %>%
     group_by(.data$region, .data$subsector) %>%
     mutate(
+      value = case_when(
+        2045 < .data$period ~ NA,
+        TRUE                ~ .data$value),
+
       value = case_when(
         ## before 2025 ----
         .data$period <  2025 ~ 0,
@@ -183,9 +201,11 @@ calcIndustry_CCS_limits <- function(
 
         'B' == .data$class & 2030 == .data$period ~
           # global 2025 subsector CCS times regional share in global subsector
-          # activity, rounded up to 1 MtCO2/a
-          max(installation_minimum,
-              .data$value.total * .data$activity / .data$activity.total),
+          # activity, rounded up to <installation_minimum> MtCO2/a, but not
+          # exceeding regional subsector emissions
+          (.data$value.total * .data$activity / .data$activity.total) %>%
+          max(installation_minimum) %>%
+          min(.data$activity * 1e3 * .data$factor, na.rm = TRUE),
 
         'C' == .data$class & 2030 == .data$period ~ 0,
 
@@ -199,9 +219,11 @@ calcIndustry_CCS_limits <- function(
 
         'C' == .data$class & 2035 == .data$period ~
           # global 2025 subsector CCS times regional share in global subsector
-          # activity, rounded up to 1 MtCO2/a
-          max(installation_minimum,
-              .data$value.total * .data$activity / .data$activity.total),
+          # activity, rounded up to <installation_minimum> MtCO2/a, but not
+          # exceeding regional subsector emissions
+          (.data$value.total * .data$activity / .data$activity.total) %>%
+          max(installation_minimum) %>%
+          min(.data$activity * 1e3 * .data$factor, na.rm = TRUE),
 
         TRUE ~ .data$value),
 
@@ -239,9 +261,9 @@ calcIndustry_CCS_limits <- function(
     select('region', 'subsector', 'period', 'value') %>%
     # expand from regions to iso3c (if different), convert unit
     full_join(
-      full_join(ind_activity_2025, region_mapping, 'iso3c'),
+      full_join(ind_activity, region_mapping, 'iso3c'),
 
-      by = c('region', 'subsector'),
+      by = c('region', 'subsector', 'period'),
       relationship = 'many-to-many'
     ) %>%
     group_by(.data$region, .data$subsector, .data$period) %>%
