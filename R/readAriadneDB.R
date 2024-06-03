e1e38612b0fd412e77cbb1f56a6bcb8d8de10c53
Falk Benke <falk.benke@pik-potsdam.de>
2024-06-03 13:42:20 +0200

    update source AriadneDB

--- R/readAriadneDB.R
+++ R/readAriadneDB.R
@@ -5,28 +5,26 @@
 #' @return A [`magpie`][magclass::magclass] object.
 #' @author Felix Schreyer
 #' @importFrom tidyr gather
-#' @importFrom rlang sym
 #' @importFrom dplyr filter %>% mutate
 #' @importFrom readxl read_excel
 
 readAriadneDB <- function() {
-  filename <- "IASA_DB_Complete_12_09_2022.xlsx"
 
   # read in file
-  data <- read_excel(filename,
+  data <- read_excel(
+    "IIASA_DB_Complete_06_03_2024.xlsx",
     sheet = "data",
     col_types = c(
-      rep("text", 6),
+      rep("text", 5),
       rep("numeric", 19)
     )
   )
 
   # rearrange and convert to magclass object
   out <- data %>%
-    gather(!!sym("period"), !!sym("value"), -!!sym("model"), -!!sym("scenario"),
-           -!!sym("region"), -!!sym("variable"), -!!sym("unit"), -!!sym("subannual")) %>%
-    filter(!is.na(!!sym("value"))) %>%
-    as.magpie(temporal = 7, spatial = 3, datacol = 8)
+    gather("period", "value", -"model", -"scenario", -"region", -"variable", -"unit") %>%
+    filter(!is.na(.data$value)) %>%
+    as.magpie(temporal = 6, spatial = 3, datacol = 7)
 
   return(out)
 }
