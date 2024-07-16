a5e90540e9af89f20b1360e1976e2f888f243d43
Falk Benke <falk.benke@pik-potsdam.de>
2024-07-16 11:25:45 +0200

    convert unit in calcCO2Prices

--- R/calcCO2Prices.R
+++ R/calcCO2Prices.R
@@ -1,7 +1,21 @@
 calcCO2Prices <- function() {
+
   # read data
   x <- readSource("ExpertGuess", subtype = "co2prices")
   getNames(x) <- NULL
+
+
+  # convert from $2005 to $2017
+  map <- toolGetMapping(type = "regional", name = "regionmappingH12.csv", where = "mappingfolder") %>%
+    select("iso3c" = "CountryCode", "region" = "RegionCode")
+
+  x <- GDPuc::convertGDP(
+    gdp = x,
+    unit_in = "constant 2005 Int$PPP",
+    unit_out = "constant 2017 Int$PPP",
+    with_regions = map,
+    replace_NAs = "regional_average"
+  )
 
   # read data used for weight
   ceds <- calcOutput("Emissions", datasource = "CEDS2024", aggregate = FALSE)
@@ -12,7 +26,7 @@ calcCO2Prices <- function() {
   return(list(
     x = x,
     weight = ceds,
-    unit = "US$2005/t CO2",
+    unit = "US$2017/t CO2",
     description = "CO2 prices in 2010, 2015 and 2020"
   ))
 }
