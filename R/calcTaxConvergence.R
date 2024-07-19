e2769c14d0c033760bf47d797bef3d3fc0bdb46d
Falk Benke <falk.benke@pik-potsdam.de>
2024-07-19 15:55:15 +0200

    convert currency in calcTaxConvergence

--- R/calcTaxConvergence.R
+++ R/calcTaxConvergence.R
@@ -9,13 +9,25 @@
 #' }
 #'
 calcTaxConvergence <- function() {
+
   # Read tax convergence levels at specific year and final energy type
   taxConvergence <- readSource("REMIND_11Regi", subtype = "taxConvergence")
+
   # average weight
-  w <- new.magpie(getRegions(taxConvergence), getYears(taxConvergence), getNames(taxConvergence), fill = 1)
-  # Return tax convergence levels aggregated to selected REMIND regions
+  w <- taxConvergence
+  w[, , ] <- 1
+
+  # convert data from $2005 to $2017
+  taxConvergence <- GDPuc::convertGDP(
+    gdp = taxConvergence,
+    unit_in = "constant 2005 Int$PPP",
+    unit_out = "constant 2017 Int$PPP",
+    replace_NAs = "with_USA"
+  )
+
   return(list(
-    x = taxConvergence, weight = w,
+    x = taxConvergence,
+    weight = w,
     unit = "$/GJ",
     description = "Tax convergence level for specific regions, year and final energy type"
   ))
