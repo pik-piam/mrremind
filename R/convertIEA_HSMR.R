4b70745325a437ec2cde01ec765be898aab5db90
Pascal Weigmann <pascal.weigmann@pik-potsdam.de>
2024-09-19 10:10:39 +0200

    fix years for hydro capacities

--- R/convertIEA_HSMR.R
+++ R/convertIEA_HSMR.R
@@ -7,8 +7,6 @@ convertIEA_HSMR <- function(x) {
 
   getItems(x, dim = 1) <- toolCountry2isocode(getItems(x, dim = 1))
   x <- toolCountryFill(x, fill = 0, verbosity = 2)
-  x[, 2030, "operational"] <- x[, 2020, "operational"]
-  x <- x[, 2030, ]
 
   return(x)
 }
