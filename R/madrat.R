d1cb58415d6b6e7b34599c7f04c5408814766150
Falk Benke <falk.benke@pik-potsdam.de>
2024-10-16 12:20:15 +0200

    move mr packages to Imports

--- R/madrat.R
+++ R/madrat.R
@@ -1,14 +1,17 @@
-
-.onAttach <- function(libname, pkgname) {
-  madrat::madratAttach(pkgname)
+.onLoad <- function(libname, pkgname) {
+  madrat::madratAttach(c(pkgname, "edgeTransport", "GDPuc", "mrcommons", "mrdrivers"))
+  madrat::setConfig(
+    nolabels = c(madrat::getConfig("nolabels"), "REMIND", "VALIDATIONREMIND")
+  )
 }
 
-.onDetach <- function(libpath) {
-  madrat::madratDetach(libpath)
+
+.onUnload <- function(libpath) {
+  madrat::madratDetach(c(libpath, "edgeTransport", "GDPuc", "mrcommons", "mrdrivers"))
 }
 
 # redirect standard messaging functions to vcat
-cat     <- function(...) vcat(1, ...)
+cat <- function(...) vcat(1, ...)
 message <- function(...) vcat(1, ...)
 warning <- function(...) vcat(0, ...)
-stop    <- function(...) vcat(-1, ...)
+stop <- function(...) vcat(-1, ...)
