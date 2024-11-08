911268a00f9fac19b30a75c1478ca30ab7d4b2f0
David Klein <dklein@pik-potsdam.de>
2024-11-08 15:30:24 +0100

    Read new biomass supplycurves from MAgPIE magpie_4.8.1 including SSP3 and unit shift (US$2017)

--- R/calcBiomassPrices.R
+++ R/calcBiomassPrices.R
@@ -4,16 +4,17 @@
 calcBiomassPrices <- function() {
 
   x <- readSource("MAgPIE", subtype = "supplyCurve_magpie_40")
+  
+  # add supply curves for SSP1-PkBudg1000 copying SSP1-PkBudg650
+  tmp <- x[, , "SSP1-SSP1-PkBudg650"]
+  getNames(tmp) <- gsub("SSP1-SSP1-PkBudg650", "SSP1-SSP1-PkBudg1000", getNames(tmp))
+  x <- mbind(x, tmp)
 
   # rename the rcp-scenarios to cm_rcp_scen switches used in REMIND
-  getNames(x) <- gsub("NDC-nocc_hist-PkBudg650",  "rcp20", getNames(x))
-  getNames(x) <- gsub("NDC-nocc_hist-PkBudg1050", "rcp26", getNames(x))
-  getNames(x) <- gsub("NDC-nocc_hist-NDC",        "rcp45", getNames(x))
-  getNames(x) <- gsub("NPI-nocc_hist-Base",       "none", getNames(x))
-
-  # rename SSP-scenarios to cm_LU_emi_scen switches used in REMIND
-  getNames(x) <- gsub("SSP2EU", "SSP2", getNames(x))
-  getNames(x) <- gsub("SDP-MC", "SDP",  getNames(x))
+  # eample: SSP2-SSP2_lowEn-PkBudg1000 -> SSP2-rcp26
+  getNames(x) <- gsub("SSP[1-5](_lowEn|)-PkBudg650",  "rcp20", getNames(x))
+  getNames(x) <- gsub("SSP[1-5](_lowEn|)-PkBudg1000", "rcp26", getNames(x))
+  getNames(x) <- gsub("SSP[1-5](_lowEn|)-NPi",        "rcp45", getNames(x))
 
   # Introduce new SSP/SDP dimension by replacing "-" with "."
   getNames(x) <- gsub("(SSP[0-9]|SDP)-", "\\1.", getNames(x))
@@ -21,24 +22,10 @@ calcBiomassPrices <- function() {
   getSets(x)["d3.2"] <- "scen2"
   getSets(x)["d3.3"] <- "char"
 
-  # add supply curves for SSP3 using the curves from SSP2
-  tmp <- x[, , "SSP2"]
-  getNames(tmp) <- gsub("SSP2", "SSP3", getNames(tmp))
-  x <- mbind(x, tmp)
-
   # if fit coefficients of a country are NA for all years (there is no supplycurve at all for this country)
   # generate artificial supplycurve with VERY high prices
   x[, , "a"][is.na(x[, , "a"])] <- 1
   x[, , "b"][is.na(x[, , "b"])] <- 0.1
-
-  # apply conversion from US$2005 to US$2017 to "a" and "b", so that the price
-  # calculated with these coefficients will be in US$2017
-  x <- GDPuc::toolConvertGDP(
-    gdp = x,
-    unit_in = "constant 2005 US$MER",
-    unit_out = mrdrivers::toolGetUnitDollar(),
-    replace_NAs = "with_USA"
-  )
 
   return(list(x           = x,
               weight      = calcOutput("FAOLand", aggregate = FALSE)[, , "6610", pmatch = TRUE][, "y2010", ],
