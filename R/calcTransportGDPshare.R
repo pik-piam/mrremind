c208533dfa5057b42ff26ef0b061894a705fbe2d
Johannes Koch <jokoch@pik-potsdam.de>
2025-02-06 10:55:54 +0100

    Refactor calcTransportGDPshare

--- R/calcTransportGDPshare.R
+++ R/calcTransportGDPshare.R
@@ -1,4 +1,4 @@
-#' @title calc transport share in GDP
+#' @title Get transport share in GDP
 #' @description provides transport share in GDP to resize edge-t transportation purchase costs
 #'
 #' @return magpie object of transport shares in GDP
@@ -9,54 +9,31 @@
 #' calcOutput("TransportGDPshare")
 #' }
 #'
-#'
-
 calcTransportGDPshare <- function() {
 
+  vars <- c("VA_Q05.Transport, storage and communication", "VA_Q05.Summation of sector GDP")
+  years <- c(2005, 2010)
+  ggdc10 <- readSource("GGDC10")[, years, vars]
 
-  ggdc10 <- readSource(type = "GGDC10") # Read GGDC 10-Sector Database
-
-  #transport share in GDP
-  share <- ggdc10[, c(2005, 2010), "VA_Q05.Transport, storage and communication"] / ggdc10[, c(2005, 2010), "VA_Q05.Summation of sector GDP"]
-  share[is.na(share)] <- 0
-
-  #Setting 2010 missing data equal to 2005
-  for (country in getItems(share, dim = 1)) {
-    if (share[country, 2010, ] == 0) {
-      share[country, 2010, ] <- share[country, 2005, ]
+  # Setting missing 2010 data equal to 2005 if available
+  for (i in getItems(ggdc10, dim = 1)) {
+    for (j in getItems(ggdc10, dim = 3)) {
+      if (ggdc10[i, 2010, j] == 0) ggdc10[i, 2010, j] <- ggdc10[i, 2005, j]
     }
   }
 
-  # calculating regional averages to fill missing data
+  # Fill missing data with regional, and then global averages
   mapH12 <- toolGetMapping(type = "regional", name = "regionmappingH12.csv", where = "mappingfolder")
-  w <- ggdc10[, c(2005, 2010), "VA_Q05.Summation of sector GDP"]
-  aggData <- toolAggregate(share, mapH12, weight = w)
-  aggData["EUR", 2010, ] <- aggData["EUR", 2005, ] # European countries are missing total 2010 values
-
-  #Setting missing data to global average numbers
   mapGLO <- mapH12
   mapGLO$RegionCode <- "GLO"
-  averageGLO <- toolAggregate(share, mapGLO, weight = w)
-  aggData[c("NEU", "REF", "CAZ"), , ] <- averageGLO["GLO", , ]
-
-  for (region in unique(mapH12$RegionCode)) {
-    for (country in mapH12[mapH12$RegionCode == region, ]$CountryCode) {
-      for (year in c(2005, 2010)) {
-        if (all(share[country, year, ] == 0)) {
-          share[country, year, ] <- aggData[region, year, ]
-      }
-      }
-    }
-  }
+  w <- ggdc10[, , "VA_Q05.Summation of sector GDP"]
+  share <- share %>%
+    toolFillWithRegionAvg(valueToReplace = 0, weight = w, regionmapping = mapH12) %>%
+    toolFillWithRegionAvg(weight = w, regionmapping = mapGLO)
 
   share <- collapseDim(share)
-
-  weight <- calcOutput("GDP", scenario = "SSP2", aggregate = FALSE, years = getYears(share, as.integer = TRUE))
+  weight <- calcOutput("GDP", scenario = "SSP2", aggregate = FALSE, years = years)
   weight <- collapseDim(weight)
 
-  # Returning capacity values
-  list(x = share,
-       weight = weight,
-       unit = "percentage",
-       description = "transport share in GDP")
+  list(x = share, weight = weight, unit = "percentage", description = "transport share in GDP")
 }
