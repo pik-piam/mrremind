ccc9176cd203aac943bcfc23717bfb7862912aec
Falk Benke <falk.benke@pik-potsdam.de>
2025-03-03 16:27:56 +0100

    fix bug in calcFedemand

--- R/calcFEdemand.R
+++ R/calcFEdemand.R
@@ -2,13 +2,13 @@
 #' @param scenario GDP and pop scenarios. Passed to [mrdrivers::calcGDP()].
 #' @author Falk Benke
 calcFEdemand <- function(scenario) {
-
   feBuildings <- calcOutput("FeDemandBuildings",
-                            subtype = "FE",
-                            scenario = scenario,
-                            warnNA = FALSE,
-                            aggregate = FALSE)
-  feIndustry <- calcOutput("FeDemandIndustry",  scenario = scenario, warnNA = FALSE, aggregate = FALSE)
+    subtype = "FE",
+    scenario = scenario,
+    warnNA = FALSE,
+    aggregate = FALSE
+  )
+  feIndustry <- calcOutput("FeDemandIndustry", scenarios = scenario, warnNA = FALSE, aggregate = FALSE)
 
   # Add up industry and buildings contributions to stationary
   stationaryItems <- c("fehes", "feh2s")
@@ -20,10 +20,12 @@ calcFEdemand <- function(scenario) {
     feStationary
   )
 
-  list(x = remind,
-       weight = NULL,
-       unit = glue::glue("EJ, except ue_cement (Gt), ue_primary_steel and ue_secondary_steel (Gt) and ue_chemicals \
+  list(
+    x = remind,
+    weight = NULL,
+    unit = glue::glue("EJ, except ue_cement (Gt), ue_primary_steel and ue_secondary_steel (Gt) and ue_chemicals \
                          and ue_otherInd ($tn)"),
-       description = "demand pathways for final energy in buildings and industry",
-       structure.data = "^(SSP[1-5].*|SDP.*)\\.(fe|ue)")
+    description = "demand pathways for final energy in buildings and industry",
+    structure.data = "^(SSP[1-5].*|SDP.*)\\.(fe|ue)"
+  )
 }
