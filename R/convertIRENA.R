e91f1fb77c82c514162beed3f630019302d3d578
lecfab <lecfab+git@gmail.com>
2025-03-26 17:27:14 +0100

    revert IRENA update in order to test wind refactoring alone

--- R/convertIRENA.R
+++ R/convertIRENA.R
@@ -4,21 +4,25 @@
 #' @param subtype data subtype. Either "Capacity" or "Generation"
 #' @return A MAgPIE object containing IRENA country disaggregated data  with
 #' historical electricity renewable capacities (MW) or generation levels (GWh)
-#' @author Renato Rodrigues, Pascal Weigmann
+#' @author Renato Rodrigues
 #' @examples
 #' \dontrun{
 #' a <- convertIRENA(x, subtype = "Capacity")
 #' }
 #'
 convertIRENA <- function(x, subtype) {
-
+  # rename countries to REMIND iso codes
+  getItems(x, dim = 1) <- gsub("\\*", "", getItems(x, dim = 1))
+  getItems(x, dim = 1) <- gsub("South Georgia",
+                               "South Georgia and the South Sandwich Islands",
+                               getItems(x, dim = 1), fixed = TRUE)
+  getItems(x, dim = 1) <- toolCountry2isocode(getItems(x, dim = 1))
   x[is.na(x)] <- 0
-
   # aggregate Kosovo to Serbia
-  x1 <- x["XKX", , ]
+  x1 <- x["KOS", , ]
   getItems(x1, dim = 1) <- c("SRB")
   x["SRB", , ] <- x["SRB", , ] + x1
-  x <- x[c("XKX"), , , invert = TRUE]
+  x <- x[c("KOS"), , , invert = TRUE]
   # fill countries with no data
   x <- toolCountryFill(x, fill = 0, verbosity = 2)
   return(x)
--- R/readIRENA.R
+++ R/readIRENA.R
@@ -1,101 +1,49 @@
 #' Read IRENA
 #'
-#' Read-in an IRENA xlsx file as magclass object
+#' Read-in an IRENA csv file as magclass object
 #'
 #' @param subtype data subtype. Either "Capacity" or "Generation"
 #' @return magpie object of the IRENA data with historical electricity renewable
 #' capacities (MW) or generation levels (GWh)
-#' @author Renato Rodrigues, Pascal Weigmann
+#' @author Renato Rodrigues
 #' @seealso \code{\link{readSource}}
 #' @examples
 #' \dontrun{
 #' a <- readSource(type = "IRENA", subtype = "Capacity")
 #' }
 #'
-#' @importFrom dplyr mutate rename select
+#' @importFrom dplyr mutate rename
 
 readIRENA <- function(subtype) {
-
-  # Reading renewables electricity capacity or generation values from xlsx
-  data <- readxl::read_xlsx("2024/IRENA_Stats_Extract_ 2024_H1_V1.xlsx",
-                            sheet = "All Data")
-
   if (subtype == "Capacity") {
-    data <- data %>%
-      mutate(value = .data$`Electricity Installed Capacity (MW)`)
+    # Reading renewables electricity capacity values in MW from csv
+    data <- utils::read.csv2("2022/Capacity.csv", sep = ";", skip = 2)
   } else if (subtype == "Generation") {
-    data <- data %>%
-      mutate(value = .data$`Electricity Generation (GWh)`)
+    # Reading renewables electricity generation values in GWh from csv
+    data <- utils::read.csv2("2022/Generation.csv", sep = ";", skip = 2)
   } else {
     stop("Not a valid subtype!")
   }
 
-  data <- data %>%
-    select(c("ISO3 code", "RE or Non-RE", "Group Technology", "Technology",
-             "Sub-Technology", "Year", "value"))
-
-  # Technology information is split over multiple columns, assemble it and
-  # calculate sums where necessary
-  data <-
-    rbind(data %>%
-            filter(.data$`RE or Non-RE` == "Total Renewable",
-                   !is.na(.data$value)) %>%
-            mutate(Technology = "Total renewable energy"),
-          data %>%
-            filter(.data$`Group Technology` %in%
-                     c("Hydropower (excl. Pumped Storage)",
-                       "Wind energy", "Bioenergy", "Solar energy",
-                       "Geothermal energy", "Marine energy"),
-                   !is.na(.data$value)) %>%
-            mutate(Technology = .data$`Group Technology`),
-          data %>%
-            filter(.data$`Technology` %in%
-                     c("Onshore wind energy", "Offshore wind energy",
-                       "Solar photovoltaic", "Liquid biofuels", "Solid biofuels",
-                       "Renewable hydropower", "Biogas", "Pumped storage",
-                       "Renewable municipal waste"),
-                   !is.na(.data$value)) %>%
-            mutate(Technology = .data$`Technology`),
-          data %>%
-            filter(.data$`Sub-Technology` %in%
-                     c("Concentrated solar power",
-                       "Other primary solid biofuels n.e.s.",
-                       "Bagasse"),
-                   !is.na(.data$value)) %>%
-            mutate(Technology = .data$`Sub-Technology`)
+  # data in wide format
+  data <- reshape2::melt(data,
+    id.vars = c(1, 2),
+    variable.name = "years", value.name = "value"
           )  %>%
-    group_by(.data$`ISO3 code`, .data$Year, .data$Technology) %>%
-    summarise(value = sum(.data$value), .groups = "drop")
+    mutate("value" = as.numeric(!!sym("value"))) %>%
+    suppressWarnings()
 
-  # harmonize Technology names with older version
-  mask <- data.frame(
-    tech_before = c("Hydropower (excl. Pumped Storage)",
-                    "Wind energy",
-                    "Solar energy",
-                    "Geothermal energy",
-                    "Marine energy",
-                    "Other primary solid biofuels n.e.s."),
-    tech_after = c("Hydropower",
-                   "Wind",
-                   "Solar",
-                   "Geothermal",
-                   "Marine",
-                   "Other solid biofuels"))
+  # rearrange column order to more readable format: year, country, tech, value (capacity or generation)
+  data <- data[, c(3, 1, 2, 4)]
 
-  for (n in 1:nrow(mask)) {
-    data[data$Technology == mask[n, 1], "Technology"] <- mask[n, 2]
-  }
+  # replacing X by y on years prefix
+  data$years <- gsub("X", "y", data$years)
 
-  # rearrange column order to more readable format:
-  # year, country, tech, value (capacity or generation)
-  data <- data[, c(2, 1, 3, 4)]
-
-  # fix name of regional column to the value it used to have before 9678353
+  # fix name of  "country" column to the value it used to have before 9678353
   data <- data %>%
     rename(`Country/area` = 2)
 
   # creating capacity or generation magpie object
   x <- as.magpie(data, temporal = 1, spatial = 2, datacol = 4)
-
   return(x)
 }
