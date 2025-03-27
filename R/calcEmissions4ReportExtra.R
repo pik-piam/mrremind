f843eb97b60db6fabe03162e8cda81116e828557
gabriel-abrahao <gabrielmabrahao@gmail.com>
2025-03-27 14:42:24 +0100

    Update documentation

--- R/calcEmissions4ReportExtra.R
+++ R/calcEmissions4ReportExtra.R
@@ -1,13 +1,11 @@
 #' Calculates historical emissions needed for estimating emission factors
 #' in extra emissions reporting
+#
 #'
-#' @param source either "PBL_2007" or "PBL_2022"
 #'
-#'
-#' @return list of magclass with REMIND input data for
-#' different sectors for timesteps 2000-2100, in Mt N per year
+#' @return list of magclass with CEDS CH4 and N2O emissionsdata for
+#' CEDS sectors for timesteps 2000-2100, in Mt N or Mt CH4 per year
 #' @author Gabriel Abrhahao
-# nolint start
 calcEmissions4ReportExtra <- function() {
   incedsall <- readSource("CEDS2024")
   # Keep just the gases we're currently interested in, but also keep all sectors
@@ -15,4 +13,3 @@ calcEmissions4ReportExtra <- function() {
 
   return(list(x = out, weight = NULL, unit = "Mt N or Mt CH4", description = "CEDS sectors emissions in 2020 for extra reporting"))
 }
-# nolint end
