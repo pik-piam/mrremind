f0fffaecd6ea6e54ab1dc486a8d5af397647ab07
gabriel-abrahao <gabrielmabrahao@gmail.com>
2025-03-27 15:26:13 +0100

    Fixing sector names and docs

--- R/calcEmissions4ReportExtra.R
+++ R/calcEmissions4ReportExtra.R
@@ -1,23 +1,26 @@
 #' Calculates historical emissions needed for estimating emission factors
 #' in extra emissions reporting
 #
-#'
+#' @param sectors: "CEDS" or "IAMC" to select the sectoral aggregation of the output
 #'
 #' @return list of magclass with CEDS CH4 and N2O emissionsdata for
-#' CEDS sectors for timesteps 2000-2100, in Mt N or Mt CH4 per year
+#' CEDS sectors for 2020, in Mt N or Mt CH4 per year
 #' @author Gabriel Abrhahao
 calcEmissions4ReportExtra <- function(sectors = "CEDS") {
   incedsall <- readSource("CEDS2024")
   # Keep just the gases we're currently interested in, but also keep all sectors
-  out <- incedsall[,2020,c("ch4","n2o_n")]
+  out <- incedsall[, 2020, c("ch4", "n2o_n")]
 
-  if (sectors == "REMIND") {
-      # Mapping between CEDS sectors and some IAMC sectors we want to estimate emission factors for
-      iamcsectormap <- toolGetMapping("mappingCEDS2024toREMIND.csv", type = "sectoral", where = "mrremind")
-      # Discard nonmapped sectors and aggregate them
-      out <- out[, , intersect(getItems(out, dim = 3.1), iamcsectormap$CEDS2024)]
-      out <- toolAggregate(out, rel = iamcsectormap, dim = 3.1, from = "CEDS2024", to = "IAMC")
+  if (sectors == "IAMC") {
+    # Mapping between CEDS sectors and some IAMC sectors we want to estimate emission factors for
+    iamcsectormap <- toolGetMapping("mappingCEDS2024toREMIND.csv", type = "sectoral", where = "mrremind")
+    # Discard nonmapped sectors and aggregate them
+    out <- out[, , intersect(getItems(out, dim = 3.1), iamcsectormap$CEDS2024)]
+    out <- toolAggregate(out, rel = iamcsectormap, dim = 3.1, from = "CEDS2024", to = "IAMC")
   }
 
-  return(list(x = out, weight = NULL, unit = "Mt N or Mt CH4", description = "CEDS sectors emissions in 2020 for extra reporting"))
+  return(list(
+    x = out, weight = NULL, unit = "Mt N or Mt CH4",
+    description = paste0("CEDS sectoral emissions in 2020 for", sectors, " sectors, used for extra reporting")
+  ))
 }
