2a788956df42dc1087822201ed0e90dd40044396
lecfab <lecfab+git@gmail.com>
2025-04-03 11:47:25 +0200

    update wind to windon in hard-coded source

--- R/readREMIND_11Regi.R
+++ R/readREMIND_11Regi.R
@@ -17,7 +17,7 @@ readREMIND_11Regi <- function(subtype) {
   switch(
     subtype,
     "tradecost"            = read.csv("LueckenDiss_TradeCost.csv", sep = ";", row.names = 1) %>% as.magpie(),
-    "deltacapoffset"       = read.csv("p_adj_deltacapoffset.csv", sep = ";")       %>% as.magpie(datacol = 2),
+    "deltacapoffset"       = read.csv("p_adj_deltacapoffset_REMIND3.4.0.csv", sep = ";")     %>% as.magpie(datacol = 2),
     "capacityFactorGlobal" = read.csv("f_cf-global_REMIND_3.4.0.csv", sep = ";")   %>% as.magpie(datacol = 2),
     "capacityFactorRules"  = read.csv("f_cf-rules_v1.2.csv", sep = ";")            %>% as.magpie(datacol = 4),
     "storageFactor"        = read.csv("storageFactor_REMIND_3.4.0.csv", sep = ";") %>% as.magpie(datacol = 2),
