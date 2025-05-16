bb0da9c4f827389a0302f0df1e6bcf2f73203c87
gabrielmabrahao <gabrielmabrahao@gmail.com>
2025-05-16 11:46:24 +0200

    Much improved NA handling

--- R/calcGAINS2025scenarios.R
+++ R/calcGAINS2025scenarios.R
@@ -1,7 +1,7 @@
 #' Calculates air pollutant emissions, activities and emission factors
 #' for all scenarios and SSPs available from GAINS, at the level
 #' of GAINS sectors.
-#' 
+#'
 #' Extrapolates activities and emissions for the 2050-2100 periods
 #' assuming a relationship between changes in polluting activities
 #' and GDP.
@@ -12,10 +12,11 @@
 #'
 #' @importFrom magclass as.magpie
 #' @importFrom tidyr pivot_longer drop_na
+#' @importFrom zoo na.approx
 calcGAINS2025scenarios <- function(subtype) {
-  require(magclass)
-  require(madrat)
-  devtools::load_all(".")
+  # require(magclass)
+  # require(madrat)
+  # devtools::load_all(".")
 
 
   # ====================================================================
@@ -23,46 +24,13 @@ calcGAINS2025scenarios <- function(subtype) {
   # ====================================================================
   allssps <- c("SSP1", "SSP2", "SSP3", "SSP4", "SSP5")
 
+  # REMIND timesteps
+  rtime <- c(seq(2005, 2055, 5), seq(2060, 2110, 10), 2130, 2150)
+
   # GAINS region mapping
   # Most of the calculations here happen at the GAINS region level, as
   # readGAINS2025 does not disaggregate its outputs
   regmap <- toolGetMapping(type = "regional", name = "regionmapping_GAINS2025.csv", where = "mrremind")
-
-  # # ====================================================================
-  # # GCI ================================================================
-  # # ====================================================================
-
-  # # Read Government Capacity Index (GCI), at the country level
-  # # Used to derivate scenario extensions
-  # ingci <- readSource("GAINS2025", subtype = "GCI")
-
-
-  # # TODO: Using GDP as aggregation weights for the moment
-  # # A more correct approach would be to have a separate aggregation
-  # # of both GCI and EFs for each pollutant, but it would require
-  # # a different approach to filling NAs and dealing with the
-  # # sector mapping before building the scenarios
-  # gdp <- calcOutput("GDP", scenario = getItems(ingci, "ssp"), aggregate = FALSE, years = getYears(ingci))
-  # getSets(gdp)[1] <- "country"
-  # getSets(gdp)[3] <- "ssp"
-  # # # Keep only countries with GCI data
-  # # gdp <- gdp[getRegions(ingci),,]
-  # str(gdp)
-
-
-  # # Aggregating GCI to GAINS regions
-  # # First we fill countries in the mapping without GCI data with region averages
-  # fillingci <- mbind(lapply(
-  #   getItems(ingci, "ssp"),
-  #   \(ssp) toolFillWithRegionAvg(ingci[, , ssp], regionmapping = regmap, callToolCountryFill = TRUE)
-  # ))
-
-  # # Then we aggregate, taking care to use only countries in the mapping
-  # gcigains <- toolAggregate(
-  #   fillingci[regmap$CountryCode, , ], regmap,
-  #   from = "CountryCode", to = "gainscode",
-  #   weight = gdp[regmap$CountryCode, , ], dim = 1, wdim = 1
-  # )
 
   # ====================================================================
   # READING GAINS DATA =================================================
@@ -80,6 +48,17 @@ calcGAINS2025scenarios <- function(subtype) {
   getItems(incle, "scenario") <- "CLE"
   getItems(inmid, "scenario") <- "SLE"
   getItems(inmfr, "scenario") <- "MFR"
+
+  # ====================================================================
+  # NA HANDLING ========================================================
+  # ====================================================================
+  # Assume activities and emissions with no data are zero for those
+  # particular region*sector*pollutant combinations
+  baseemi[is.na(baseemi)] <- 0.0
+  baseact[is.na(baseact)] <- 0.0
+
+  # EF input files are a bit messier, so only assume zeroes after
+  # the time dimension is handled properly
 
   # ====================================================================
   # EF SCENARIO ASSUMPTIONS ============================================
@@ -107,28 +86,41 @@ calcGAINS2025scenarios <- function(subtype) {
 
   # Concatenating scenarios ==============================================================
   efs <- mbind(cle, mfr, sle)
-  # Dropping odd sectors in the files that have no data 
-  efs <- efs[,,c(" ", "Power_Gen_HLF_CCS", "Unattributed"), invert = T]
+  # Dropping odd sectors in the files that have no data
+  efs <- efs[, , c(" ", "Power_Gen_HLF_CCS", "Unattributed"), invert = T]
 
   # Concatenating historical EFs to all scenarios ========================================
-  histefs <- collapseDim(incle[,setdiff(getYears(incle), getYears(efs)),"historical"])
+  histefs <- collapseDim(incle[, setdiff(getYears(incle), getYears(efs)), "historical"])
   histefs <- mbind(lapply(allssps, \(ssp) add_dimension(histefs, 3.1, add = "ssp", nm = ssp)))
-  histefs <- mbind(lapply(getItems(efs,"scenario"), \(scen) add_dimension(histefs, 3.1, add = "scenario", nm = scen)))
-  histefs <- histefs[,,c("Unattributed"), invert = T]
+  histefs <- mbind(lapply(getItems(efs, "scenario"), \(scen) add_dimension(histefs, 3.1, add = "scenario", nm = scen)))
+  histefs <- histefs[, , c("Unattributed"), invert = T]
 
+  # 2025 tends to have no data in historical or scenario either, so interpolate
+  # between 2020 (historical) and 2030 (scenario)
   efs <- mbind(histefs, efs)
+  gyears <- getYears(efs)
+  efs <- efs[, 2025, , invert = T]
+  efs <- toolFillYears(efs, gyears)
+
+  # NA handling in EFs ====================================================================
+  # The goal is to fill every EF with something, at least zero
+  # If there's some EF reported in any timestep of 
+  # that activity*region*scenario, fill with the temporally closest
+  # one, otherwise fill with zero
+  efs <- toolFillYearsWithClosest(efs)
+  efs[is.na(efs)] <- 0
 
   # ====================================================================
   # EMISSIONS ==========================================================
-  # ====================================================================  
+  # ====================================================================
 
   # Extending activities between 2050 and 2100
-  # The assumption here is that GDP growth is a proxy for polluting 
+  # The assumption here is that GDP growth is a proxy for polluting
   # activities. That relationship can be positive or negative, and is
   # modelled with an elasticity factor derived from the GAINS baseline.
 
   # Reading GDP data from madrat for all SSPs and aggregating to
-  # GAINS regions. 
+  # GAINS regions.
   gdp <- calcOutput("GDP", scenario = allssps, aggregate = F)
   gdpgains <- toolAggregate(
     gdp, regmap,
@@ -164,23 +156,77 @@ calcGAINS2025scenarios <- function(subtype) {
   sspact <- toolFillYears(sspact, years = getYears(efs))
 
   # Remove sectors absent in efs
-  sspact <- sspact[,,getItems(efs, "sectorGAINS")]
+  sspact <- sspact[, , getItems(efs, "sectorGAINS")]
 
   # Extending emissions by applying EFs to activity levels
   # Note that none of those will be the same as the baseline, as the baseline scenario
   # is not included (CLE:current legislation is the least strict one)
   sspemi <- efs * sspact
-  
+
   # ====================================================================
-  # REGIONAL DISAGGREGATION =============================================
-  # ====================================================================  
+  # COUNTRY DISAGGREGATION =============================================
+  # ====================================================================
   # Up to this point, it was simpler to deal with everything at the level
   # of GAINS regions. But with emissions and EFs calculated, we have to
-  # pick the right disaggregation weights
+  # pick the right disaggregation weights, keeping in mind EFs are
+  # intensive.
 
-  # inceds <- readSource("CEDS2025")
-  # str(inceds)
-  # getItems(inceds,"pollutant")
+  # Assume constant emissions, activities and EFs after 2100
 
+  # Use CEDS 2020 emissions as disaggregation weights for emissions
+  # and activities
+  inceds <- calcOutput("AirPollEmiRef", baseyear = 2020, aggregate = F)
 
+  if (subtype == "emissions") {
+    # Emissions: Weighted by CEDS2020 Emissions in disaggregation,
+    # no weights for aggregation (sum)
+    csspemi <- toolAggregate(
+      sspemi, regmap,
+      from = "gainscode", to = "CountryCode",
+      weight = inceds, dim = 1, wdim = 1
+    )
+    outsspemi <- time_interpolate(csspemi, rtime, extrapolation_type = "constant")
+
+    out <- outsspemi
+    wgt <- NULL
+    unit <- "Mt/yr"
+  } else if (subtype %in% c("activities", "emission_factors")) {
+    # We also need to calculate activities to use as weights when
+    # asked for emissions factors
+
+    # Activities: Weighted by CEDS2020 Emissions in disaggregation,
+    # no weights for aggregation (sum)
+    csspact <- toolAggregate(
+      sspact, regmap,
+      from = "gainscode", to = "CountryCode",
+      weight = inceds, dim = 1, wdim = 1
+    )
+
+    outsspact <- time_interpolate(csspact, rtime, extrapolation_type = "constant")
+
+    out <- outsspact
+    wgt <- NULL
+    unit <- "PJorMt/yr"
+    if (subtype == "emission_factors") {
+      # EFs: No disaggregation weights, intensive,
+      # but carry activities as aggregation weights
+      csspefs <- toolAggregate(
+        efs, regmap,
+        from = "gainscode", to = "CountryCode",
+        weight = NULL, dim = 1, wdim = 1
+      )
+      outsspefs <- time_interpolate(csspefs, rtime, extrapolation_type = "constant")
+
+      out <- outsspefs
+      wgt <- outsspact
+      unit <- "Mt/PJorMt"
+    }
+  }
+
+  return(list(
+    x = out,
+    weight = wgt,
+    unit = unit,
+    description = "Scenario for emissions or emission factors calculated based on GAINS2025 data."
+  ))
 }
