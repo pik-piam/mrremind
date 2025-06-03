9acde2fc1c45666408097d5e8e46803f3142d856
Falk Benke <falk.benke@pik-potsdam.de>
2025-06-03 11:23:54 +0200

    refactor convertUNFCCC_NDC

--- R/convertUNFCCC_NDC.R
+++ R/convertUNFCCC_NDC.R
@@ -16,7 +16,6 @@
 convertUNFCCC_NDC <- function(x, subtype, subset = NULL) { # nolint: object_name_linter.
 
   if (grepl("Capacity", subtype, fixed = TRUE)) {
-
     # TODO: do we want to implement FE-Production-Share?
 
     # pre-processing ----
@@ -31,28 +30,31 @@ convertUNFCCC_NDC <- function(x, subtype, subset = NULL) { # nolint: object_name
       x[is.na(x)] <- uncond[is.na(x)]
     }
 
-    x[is.na(x)] <- 0
-
     # generate target years, at least to 2035, but allow every year in x to be rounded up to next fiver
     targetYears <- seq(2020, max(2035, 4 + max(getYears(x, as.integer = TRUE))), by = 5)
 
     # generate new object x_target that has values only for targetYears and transfer those from x
-    x_target <- new.magpie(getItems(x, dim = 1), targetYears, getNames(x), fill = 0)
+    x_target <- new.magpie(getItems(x, dim = 1), targetYears, getNames(x), fill = NA)
     x_target[, intersect(getYears(x), getYears(x_target)), ] <- x[, intersect(getYears(x), getYears(x_target)), ]
 
-    # for non-fiver years, transfer them to following fiver year, increasing every year by 5 percentage points
-    # resolve conflicts by using the higher value
     for (i in getYears(x, as.integer = TRUE)) {
       if (i %% 5 != 0) {
-        x_target[, i - (i %% 5) + 5, ] <- apply(
-          matrix(c(as.vector(x_target[, i - (i %% 5) + 5, ]),
-                   as.vector(x[, i, ]) * (1 + (5 - (i %% 5)) * 0.05)), ncol = 2), 1, max
-        )
+        # for non-fiver years, transfer them to following fiver year, increasing every year by 5 percentage points
+        x[, i, ] <- x[, i, ] * (1 + (5 - (i %% 5)) * 0.05)
+        j <- i - (i %% 5) + 5
+        if (j %in% getYears(x, as.integer = TRUE)) {
+          # if there is already a value for the year, use the higher value
+          x_target[, j, ] <- pmax(x[, i, ], x[, j, ], na.rm = TRUE)
+        } else {
+          x_target[, j, ] <- x[, i, ]
+        }
       }
     }
 
     x_capacity <- new.magpie(getItems(x_target, dim = 1), getItems(x_target, dim = 2),
-                             getNames(x_target, dim = 2), fill = 0)
+      getNames(x_target, dim = 2),
+      fill = NA
+    )
 
     # gather reference capacities (2015) ----
     # x_ref contains 2015 capacities except for hydro where current generation values are taken
@@ -61,6 +63,9 @@ convertUNFCCC_NDC <- function(x, subtype, subset = NULL) { # nolint: object_name
 
     hist_cap <- readSource("IRENA", subtype = "Capacity") / 1000 # Units in GW
     hist_gen <- readSource("IRENA", subtype = "Generation") # Units in GWh
+
+    # capacity factors determine how much electricity (GWh/yr) you can produce from 1 GW of capacity
+    # the lower the number, the more electricity can be produced
 
     # Real world capacity factor for hydro = Generation in 2015 / Capacity in last 2015
     # using cf_hydro_realworld directly causes converges errors because some are very small
@@ -90,7 +95,7 @@ convertUNFCCC_NDC <- function(x, subtype, subset = NULL) { # nolint: object_name
     # 0/1 mask to only initialize cells with targets in any year
     # this is most likely a correction of the values coming from BP,
     # which in a few cases are the result of disaggregating regions (e.g. Other CIS)
-    mask <- dimSums(x_target, dim = c(2, 3.1))[, , "Nuclear"]
+    mask <- dimSums(x_target, dim = c(2, 3.1), na.rm = TRUE)[, , "Nuclear"]
     mask[mask != 0] <- 1
 
     x_ref[, , "Nuclear"] <- hist_cap_nuclear * mask
@@ -110,38 +115,28 @@ convertUNFCCC_NDC <- function(x, subtype, subset = NULL) { # nolint: object_name
     x_capacity_abs[, , "Hydro"] <- x_ref[, , "Hydro"] +
       x_target[, , "AC-Absolute.Hydro", drop = TRUE] * cf_hydro * 8760
 
-
     # handle Target Type 'TIC-Absolute' ----
     x_capacity_tic <- x_capacity
 
-    # total installed capacity targets are the maximum of 2015 capacity and capacity in target year
-    # TODO: possibly revise, as this might no longer make sense as we introduce new technologies (e.g. fossil technologies)
-    x_capacity_tic[, , c("Nuclear", "Biomass", "Wind", "Solar")] <- pmax(
-      x_ref[, , c("Nuclear", "Biomass", "Wind", "Solar")],
-      x_target[, , c("Nuclear", "Biomass", "Wind", "Solar")][, , "TIC-Absolute", drop = TRUE]
-    )
+    # total installed capacity targets are already in the right format
+    x_capacity_tic[, , c("Nuclear", "Biomass", "Wind", "Solar")] <- x_target[, , c("Nuclear", "Biomass", "Wind", "Solar")][, , "TIC-Absolute", drop = TRUE]
 
     # for Hydro, this is production (GWh/yr)
-    x_capacity_tic[, , "Hydro"] <- pmax(
-      x_ref[, , "Hydro"],
-      # TODO: Isn't the conversion capacity to production missing multiplication with 8760?
-      x_target[, , "TIC-Absolute.Hydro", drop = TRUE] * cf_hydro
-    )
+    # TODO: Isn't the conversion capacity to production missing multiplication with 8760?
+    x_capacity_tic[, , "Hydro"] <- x_target[, , "TIC-Absolute.Hydro", drop = TRUE] * cf_hydro
 
     # handle Target Type 'Production-Absolute' for Biomass and Nuclear ----
     x_capacity_prod <- x_capacity
 
     # converting production targets (GWh) to absolute capacity targets (GW)
 
-    # always take the higher value from 2015 capacity and capacity in target year, calculated from production
-    x_capacity_prod[, , "Nuclear"] <- pmax(x_ref[, , "Nuclear"],
-                                           x_target[, , "Production-Absolute.Nuclear"] / (8760 * cf_nuclear))
-    x_capacity_prod[, , "Biomass"] <- pmax(x_ref[, , "Biomass"],
-                                           x_target[, , "Production-Absolute.Biomass"] / (8760 * cf_biomass))
+    # capacity in target year, calculated from production
+    x_capacity_prod[, , "Nuclear"] <- x_target[, , "Production-Absolute.Nuclear"] / (8760 * cf_nuclear)
+    x_capacity_prod[, , "Biomass"] <- x_target[, , "Production-Absolute.Biomass"] / (8760 * cf_biomass)
 
     # handle Target Type 'Production-Absolute' for Solar, Wind, and Hydro ----
 
-    # obtain the capacity factors (nur) values and associated maximum production (maxprod) for Hydro, Wind, and Solar
+    # obtain the capacity factors (nur) and associated maximum production (maxprod) for Hydro, Wind, and Solar
     # the quality is a continuous number (1-9 for Solar/ Wind and 1-5 for Hydro) that is binned onto capacity factors,
     # each with a limited installation potential, the lower the number, the higher the capacity factor
     potentialWind <- calcOutput("PotentialWindOn", aggregate = FALSE)
@@ -167,20 +162,23 @@ convertUNFCCC_NDC <- function(x, subtype, subset = NULL) { # nolint: object_name
     potential[, , "maxprod"] <- potential[, , "maxprod"] * 277777.778
     potential[is.na(potential)] <- 0
 
-    # for Hydro, any TIC and AC Absolute targets are converted to GWh/yr
-    # and copied over to Production-Absolute
+    # for Hydro, any TIC and AC Absolute targets are converted to Production-Absolute targets in GWh/yr
+    # Production-Absolute is the max of TIC, AC, Production-Absolute, and 2015 reference
+    # TODO: why not do this just like for other Renewables? why special treatment for Hydro?
+    # TODO: why do the complex calculation below for reference values as well?
     x_target[, , "Production-Absolute.Hydro"] <- pmax(x_target[, , "Production-Absolute.Hydro"],
-                                                      x_capacity_tic[, , "Hydro"],
-                                                      x_capacity_abs[, , "Hydro"])
+      x_capacity_tic[, , "Hydro"],
+      x_capacity_abs[, , "Hydro"],
+      x_ref[, , "Hydro"],
+      na.rm = TRUE
+    )
 
-
-    # drop the target for all Production-Absolute Hydro targets with maxprod
-    # all-zero or at least one negative maxprod value,
+    # drop the target for all Production-Absolute Hydro targets with maxprod all-zero,
+    # or at least one negative maxprod value
     for (r in getItems(x_target, dim = 1)) {
       if (any(x_target[r, , "Production-Absolute.Hydro"] != 0) &&
-          all(potential[r, , "Hydro.maxprod"] == 0) || any(potential[r, , "Hydro.maxprod"] < 0)) {
-        message(subtype, "Dropping target(s) of type 'Production-Absolute' for Hydro in country for ", r,
-                ", as it has zero or negative maxprod.")
+        (all(potential[r, , "Hydro.maxprod"] == 0) || any(potential[r, , "Hydro.maxprod"] < 0))) {
+        message(subtype, ": Dropping target(s) of type 'Production-Absolute' for Hydro in country for ", r, ", as it has zero or negative maxprod.")
         x_target[r, , "Production-Absolute.Hydro"] <- 0
       }
     }
@@ -191,6 +189,7 @@ convertUNFCCC_NDC <- function(x, subtype, subset = NULL) { # nolint: object_name
     regions <- numeric(length(getItems(x_target, dim = 1)))
     names(regions) <- getItems(x_target, dim = 1)
 
+
     # TODO: understand and document this
     for (t in c("Solar", "Wind", "Hydro")) {
       pot <- potential[, , t]
@@ -200,48 +199,56 @@ convertUNFCCC_NDC <- function(x, subtype, subset = NULL) { # nolint: object_name
         regions[] <- 0
         for (r in getItems(tar, dim = 1)) {
           tmp_target <- numeric(10)
-          # target exists, maxprod exceeds maximum target
-          if (!R.utils::isZero(tar[r, y, "Production-Absolute"]) &&
-                dimSums(pot[r, , "maxprod"], na.rm = TRUE) > max(tar[r, , "Production-Absolute"])) {
-
-            # extracting the first non-zero location of maxprod
-            loc <- min(which(!R.utils::isZero(pot[r, , "maxprod"])))
-            tmp_target[1] <- tar[r, y, "Production-Absolute"]
-            # maxprod in first location already exceeds maximum target
-            if (pot[r, , "maxprod"][, , loc] > tmp_target[1]) {
-              # convert production to capacity using specific capacity factor
-              regions[r] <- tmp_target[1] / (8760 * pot[r, , "nur"][, , loc])
-            } else {
-              tmp_target[2] <- tmp_target[1] - pot[r, , "maxprod"][, , loc]
-              if (pot[r, , "maxprod"][, , loc + 1] > tmp_target[2]) {
-                regions[r] <- (1 / 8760) * (data_in_use[r, , ][, , loc] + tmp_target[1] / pot[r, , "nur"][, , loc + 1])
+          # target exists
+          if (!is.na(tar[r, y, "Production-Absolute"]) && !R.utils::isZero(tar[r, y, "Production-Absolute"])) {
+            # cumulated maxprod potential exceeds largest target for that region
+            if (dimSums(pot[r, , "maxprod"], na.rm = TRUE) > max(tar[r, , "Production-Absolute"], na.rm = TRUE)) {
+              # extracting the first non-zero location of maxprod
+              loc <- min(which(!R.utils::isZero(pot[r, , "maxprod"])))
+              tmp_target[1] <- tar[r, y, "Production-Absolute"]
+              # maxprod for best capacity factor exceeds target
+              if (pot[r, , "maxprod"][, , loc] > tmp_target[1]) {
+                # convert production to capacity using best capacity factor
+                regions[r] <- tmp_target[1] / (8760 * pot[r, , "nur"][, , loc])
               } else {
-                tmp_target[3] <- tmp_target[2] - pot[r, , "maxprod"][, , loc + 1]
-                if (pot[r, , "maxprod"][, , loc + 2] > tmp_target[3]) {
-                  regions[r] <- (1 / 8760) * (data_in_use[r, , ][, , loc] + data_in_use[r, , ][, , loc + 1]
-                                              + tmp_target[2] / pot[r, , "nur"][, , loc + 2])
+                tmp_target[2] <- tmp_target[1] - pot[r, , "maxprod"][, , loc]
+                if (pot[r, , "maxprod"][, , loc + 1] > tmp_target[2]) {
+                  # TODO this is most likely wrong, should be tmp_target[2]
+                  regions[r] <- (1 / 8760) * (data_in_use[r, , ][, , loc] + tmp_target[1] / pot[r, , "nur"][, , loc + 1])
                 } else {
-                  tmp_target[4] <- tmp_target[3] - pot[r, , "maxprod"][, , loc + 2]
-                  if (pot[r, , "maxprod"][, , loc + 3] > tmp_target[4]) {
-                    regions[r] <- (1 / 8760) * (data_in_use[r, , ][, , loc] + data_in_use[r, , ][, , loc + 1] + data_in_use[r, , ][, , loc + 2] +
-                                                  tmp_target[3] / pot[r, , "nur"][, , loc + 3])
-                    regions[r] <- tmp_target[1]
+                  # so far, we never get to beyond point in the calculation
+                  tmp_target[3] <- tmp_target[2] - pot[r, , "maxprod"][, , loc + 1]
+                  if (pot[r, , "maxprod"][, , loc + 2] > tmp_target[3]) {
+                    regions[r] <- (1 / 8760) * (data_in_use[r, , ][, , loc] + data_in_use[r, , ][, , loc + 1]
+                      + tmp_target[2] / pot[r, , "nur"][, , loc + 2])
                   } else {
-                    tmp_target[5] <- tmp_target[4] - pot[r, , "maxprod"][, , loc + 3]
-                    if (pot[r, , "maxprod"][loc + 4] > tmp_target[5]) {
+                    tmp_target[4] <- tmp_target[3] - pot[r, , "maxprod"][, , loc + 2]
+                    if (pot[r, , "maxprod"][, , loc + 3] > tmp_target[4]) {
                       regions[r] <- (1 / 8760) * (data_in_use[r, , ][, , loc] + data_in_use[r, , ][, , loc + 1] + data_in_use[r, , ][, , loc + 2] +
-                                                    data_in_use[r, , ][, , loc + 3] + tmp_target[4] / pot[r, , "nur"][, , loc + 4])
+                        tmp_target[3] / pot[r, , "nur"][, , loc + 3])
+                      regions[r] <- tmp_target[1]
                     } else {
-                      tmp_target[6] <- tmp_target[5] - pot[r, , "maxprod"][, , loc + 4]
-                      if (pot[r, , "maxprod"][loc + 5] > tmp_target[6]) {
+                      tmp_target[5] <- tmp_target[4] - pot[r, , "maxprod"][, , loc + 3]
+                      if (pot[r, , "maxprod"][loc + 4] > tmp_target[5]) {
                         regions[r] <- (1 / 8760) * (data_in_use[r, , ][, , loc] + data_in_use[r, , ][, , loc + 1] + data_in_use[r, , ][, , loc + 2] +
-                                                      data_in_use[r, , ][, , loc + 3] + data_in_use[r, , ][, , loc + 4] +
-                                                      tmp_target[5] / pot[r, , "nur"][, , loc + 5])
+                          data_in_use[r, , ][, , loc + 3] + tmp_target[4] / pot[r, , "nur"][, , loc + 4])
+                      } else {
+                        tmp_target[6] <- tmp_target[5] - pot[r, , "maxprod"][, , loc + 4]
+                        if (pot[r, , "maxprod"][loc + 5] > tmp_target[6]) {
+                          regions[r] <- (1 / 8760) * (data_in_use[r, , ][, , loc] + data_in_use[r, , ][, , loc + 1] + data_in_use[r, , ][, , loc + 2] +
+                            data_in_use[r, , ][, , loc + 3] + data_in_use[r, , ][, , loc + 4] +
+                            tmp_target[5] / pot[r, , "nur"][, , loc + 5])
+                        }
                       }
                     }
                   }
                 }
               }
+            } else {
+              message(
+                subtype, ": Dropping ", y, " target of type 'Production-Absolute' for ", t,
+                " in ", r, ", as it exceeds maxprod potential."
+              )
             }
           }
         }
@@ -251,22 +258,24 @@ convertUNFCCC_NDC <- function(x, subtype, subset = NULL) { # nolint: object_name
 
     # merge capacities ---
 
-    # take the maximum of
+    # take the maximum of all target types (usually, only one is target is given)
+    # and the 2015 reference value
     x_capacity[, , c("Solar", "Wind", "Biomass", "Nuclear")] <- pmax(
-
       x_capacity_abs[, , c("Solar", "Wind", "Biomass", "Nuclear")],
       x_capacity_tic[, , c("Solar", "Wind", "Biomass", "Nuclear")],
-      x_capacity_prod[, , c("Solar", "Wind", "Biomass", "Nuclear")]
+      x_capacity_prod[, , c("Solar", "Wind", "Biomass", "Nuclear")],
+      x_ref[, , c("Solar", "Wind", "Biomass", "Nuclear")],
+      na.rm = TRUE
     )
 
-    # TIC and AC-Absolute for Hydro have been included in previous steps already
+    # TIC and AC-Absolute for Hydro and 2015 reference have been included in previous steps already
     x_capacity[, , "Hydro"] <- x_capacity_prod[, , "Hydro"]
 
     # make sure that targets in subsequent years are always same or greater
     for (r in getItems(x_target, dim = 1)) {
       for (t in getNames(x_capacity)) {
         for (i in utils::head(targetYears, -1)) {
-          if (x_capacity[r, i + 5, t] < x_capacity[r, i, t]) {
+          if (is.na(x_capacity[r, i + 5, t]) || x_capacity[r, i + 5, t] < x_capacity[r, i, t]) {
             x_capacity[r, i + 5, t] <- x_capacity[r, i, t]
           }
         }
@@ -277,7 +286,7 @@ convertUNFCCC_NDC <- function(x, subtype, subset = NULL) { # nolint: object_name
     otherRegions <- setdiff(getItems(hist_cap, dim = 1), getItems(x_capacity, dim = 1))
 
     x_other <- new.magpie(otherRegions, targetYears, getNames(x_capacity))
-    x_other[, , c("Wind", "Solar")]  <- hist_cap[otherRegions, 2015, c("Solar", "Wind")]
+    x_other[, , c("Wind", "Solar")] <- hist_cap[otherRegions, 2015, c("Solar", "Wind")]
     x_other[, , "Biomass"] <- hist_cap[otherRegions, 2015, "Bioenergy"]
     x_other[, , "Hydro"] <- hist_cap[otherRegions, 2015, "Renewable hydropower"] * cf_hydro
     x_other[, , "Nuclear"] <- 0
@@ -286,10 +295,11 @@ convertUNFCCC_NDC <- function(x, subtype, subset = NULL) { # nolint: object_name
     x[is.na(x)] <- 0
 
     # rename technologies according to REMIND ----
-    m <- c("Wind" = "wind", "Solar" = "spv", "Biomass" = "bioigcc",
-           "Nuclear" = "tnrs", "Hydro" = "hydro")
+    m <- c(
+      "Wind" = "wind", "Solar" = "spv", "Biomass" = "bioigcc",
+      "Nuclear" = "tnrs", "Hydro" = "hydro"
+    )
     getNames(x) <- m[getNames(x)]
-
   }
 
   if (grepl("Emissions", subtype, fixed = TRUE)) {
