2e56bf4205497a2ed893a1a6ed29d9ba086fc978
gabrielmabrahao <gabrielmabrahao@gmail.com>
2025-06-10 18:21:26 +0200

    Dropping global and formatting

--- R/readIMAGE2025.R
+++ R/readIMAGE2025.R
@@ -4,12 +4,18 @@
 #' @author Gabriel Abrahao
 #' @importFrom readxl read_excel
 readIMAGE2025 <- function() {
-    indata <- read_excel("F_gas_data_PIK.xlsx")
-    indata <- tidyr::pivot_longer(indata, 6:25, names_to = "period", values_to = "value")
-    colnames(indata) <- tolower(colnames(indata))
-    # There are several duplicated scenarios in the original data, so we
-    # keep only the last one as the defaultt as.magpie behaviour, and suppress warnings
-    mdata <- suppressWarnings(as.magpie(indata, spatial = "region", temporal = "period"))
+  # Read raw data from Excel file sent by Matthias Harmsen in early 2025
+  # as part of the ScenarioMIP effort
+  indata <- read_excel("F_gas_data_PIK.xlsx")
+  indata <- tidyr::pivot_longer(indata, 6:25, names_to = "period", values_to = "value")
+  colnames(indata) <- tolower(colnames(indata))
+  
+  # There are several duplicated scenarios in the original data, so we
+  # keep only the last one as the defaultt as.magpie behaviour, and suppress warnings
+  mdata <- suppressWarnings(as.magpie(indata, spatial = "region", temporal = "period"))
 
-    return(mdata)
+  # Drop global region
+  mdata <- mdata["World", , invert = TRUE]
+
+  return(mdata)
 }
