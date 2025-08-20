1f77d163ee48c64b96e7c997c422d2b825a6c978
Falk Benke <falk.benke@pik-potsdam.de>
2025-08-20 16:14:54 +0200

    move weight handling to custom aggregation function

--- R/calcEDGETransport.R
+++ R/calcEDGETransport.R
@@ -4,11 +4,9 @@
 #'
 #' @param subtype  REMIND/iterative EDGE-T input data subtypes
 #' @return REMIND/iterative EDGE-T input data for all scenario combinations
-#' @examples
-#' \dontrun{
-#' a <- calcOutput(type = "EDGETransport", subtype = "CAPEXandNonFuelOPEX", aggregate = F)
-#' }
+#'
 calcEDGETransport <- function(subtype) {
+
   x <- readSource("EDGETransport", subtype)
 
   weightSum <- value <- NULL
@@ -16,50 +14,61 @@ calcEDGETransport <- function(subtype) {
   switch(subtype,
     "f35_esCapCost" = {
       weight <- readSource("EDGETransport", subtype = "weightESdemand")
-      # Rule out numerical errors after disaggregating very small numbers
       weight[weight < 1e-5] <- 0
-      # check whether weightsum is zero for some cases
-      # if so, the values should just be aggregated equally in order to prevent zeros in the results
-      weight <- rmndt::magpie2dt(weight)
-      regMap <- toolGetMapping(getConfig("regionmapping"), type = "regional", where = "mappingfolder")
-      data.table::setnames(regMap, "CountryCode", "all_regi")
-      weight <- merge(weight, regMap[, c("all_regi", "RegionCode")], by = "all_regi")
-      weight[, weightSum := sum(value), by = c("RegionCode", "GDP_scenario", "DEM_scenario", "EDGE_scenario", "all_teEs", "tall")]
-      weight[weightSum == 0, value := 1]
-      weight[, c("RegionCode", "weightSum") := NULL]
-      weight <- as.magpie(weight)
       unit <- "2017US$/(p|t)km"
       description <- "Capital cost (purchase) per energy service demand on CES level."
-      aggregationArguments <- list(zeroWeight = "allow")
+
+      aggregationFunction <- function(x, rel, weight) {
+
+        # check whether weightsum is zero for some cases
+        # if so, the values should just be aggregated equally in order to prevent zeros in the results
+        weight <- rmndt::magpie2dt(weight)
+        regMap <- rel
+        data.table::setnames(regMap, "country", "all_regi")
+        weight <- merge(weight, regMap[, c("all_regi", "region")], by = "all_regi")
+        weight[, weightSum := sum(value), by = c("region", "GDP_scenario", "DEM_scenario",
+                                                 "EDGE_scenario", "all_teEs", "tall")]
+        weight[weightSum == 0, value := 1]
+        weight[, c("region", "weightSum") := NULL]
+        weight <- as.magpie(weight)
+        toolAggregate(x, rel = rel, weight)
+      }
+
     },
     "f35_fe2es" = {
       weight <- readSource("EDGETransport", "f35_demByTech")
       weight[weight < 1e-5] <- 0
-      # check whether weightsum is zero for some cases
-      # if so, the values should just be aggregated equally in order to prevent zeros in the results
-      weight <- rmndt::magpie2dt(weight)
-      regMap <- toolGetMapping(getConfig("regionmapping"), type = "regional", where = "mappingfolder")
-      data.table::setnames(regMap, "CountryCode", "all_regi")
-      weight <- merge(weight, regMap[, c("all_regi", "RegionCode")], by = "all_regi")
-      weight[, weightSum := sum(value), by = c("RegionCode", "GDP_scenario", "DEM_scenario", "EDGE_scenario", "all_enty", "all_in", "all_teEs", "tall")]
-      weight[weightSum == 0, value := 1]
-      weight[, c("RegionCode", "weightSum", "all_enty", "all_in") := NULL]
-      weight <- as.magpie(weight)
       unit <- "trn (p|t)km/Twa"
       description <- "Energy efficiency on CES level."
-      aggregationArguments <- list(zeroWeight = "allow")
+
+      aggregationFunction <- function(x, rel, weight) {
+
+        # check whether weightsum is zero for some cases
+        # if so, the values should just be aggregated equally in order to prevent zeros in the results
+        weight <- rmndt::magpie2dt(weight)
+        regMap <- rel
+        data.table::setnames(regMap, "country", "all_regi")
+        weight <- merge(weight, regMap[, c("all_regi", "region")], by = "all_regi")
+        weight[, weightSum := sum(value), by = c("region", "GDP_scenario", "DEM_scenario",
+                                                 "EDGE_scenario", "all_enty", "all_in", "all_teEs", "tall")]
+        weight[weightSum == 0, value := 1]
+        weight[, c("region", "weightSum", "all_enty", "all_in") := NULL]
+        weight <- as.magpie(weight)
+        toolAggregate(x, rel = rel, weight)
+      }
+
     },
     "f35_demByTech" = {
       weight <- NULL
       unit <- "TWa"
       description <- "Final energy demand on CES level."
-      aggregationArguments <- list()
+      aggregationFunction = madrat::toolAggregate
     },
     "f29_trpdemand" = {
       weight <- NULL
       unit <- "trillion pkm/trillion tkm"
       description <- "Energy service demand on CES level."
-      aggregationArguments <- list()
+      aggregationFunction = madrat::toolAggregate
     }
   )
 
@@ -68,6 +77,6 @@ calcEDGETransport <- function(subtype) {
     weight = weight,
     unit = unit,
     description = description,
-    aggregationArguments = aggregationArguments
+    aggregationFunction = aggregationFunction
   )
 }
