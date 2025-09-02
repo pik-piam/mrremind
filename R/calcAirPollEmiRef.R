00bee9025dd08ebaba40af5c8859e05c136d25a9
gabrielmabrahao <gabrielmabrahao@gmail.com>
2025-09-02 16:26:20 +0200

    New subtype for INTERMEDIARY

--- R/calcAirPollEmiRef.R
+++ R/calcAirPollEmiRef.R
@@ -2,7 +2,8 @@
 #' in combination with GAINS data at different sectoral aggregations
 #'
 #' @param subtype total per pollutant ("total"), raw CEDS sectors ("sectorsCEDSraw"),
-#' aggregated to CEDS16 ("sectorsCEDS16") or mapped to GAINS2025 sectors ("sectorsGAINS2025")
+#' aggregated to CEDS16 ("sectorsCEDS16"), mapped to GAINS2025 sectors ("sectorsGAINS2025")
+#' or mapped to the INTERMEDIARY sectors used on the GAINS mapping ("sectorsINTERMEDIARY")
 #' @param baseyear year to take as a reference from CEDS, ignored for the EDGAR2005 LUC CO2 emissions
 #' @param outunits "Mt/yr" or "kt/yr"
 #' @param namesformat "GAINS2025" or "REMIND" or "REMINDexo", the standard to use for pollutant names
@@ -104,7 +105,7 @@ calcAirPollEmiRef <- function(
     out <- setYears(fullceds16)
     unit <- outunits
     desc <- paste0("Emissions in year ", baseyear)
-  } else if (subtype == "sectorsGAINS2025") {
+  } else if (subtype %in% c("sectorsGAINS2025", "sectorsINTERMEDIARY")) {
     # CEDS Sectoral mapping to aggregated GAINS sectors
     gainsmap <- toolGetMapping(type = "sectoral", name = "mappingCEDS62toGAINSsectors.csv", where = "mrremind")
 
@@ -137,73 +138,94 @@ calcAirPollEmiRef <- function(
     gainsemi[is.na(gainsemi)] <- 0
 
     # If we're using a different pollutant names setup, fixPolNames should also work in gainsemi
-    if(namesformat != "GAINS2025") {
+    if (namesformat != "GAINS2025") {
       gainsemi <- fixPolNames(gainsemi)
     }
 
-    # Rename the "sector dimension to match that used in other functions
+    # Rename the sector dimension to match that used in other functions
     # that operate on GAINS
     getSets(fullceds)[getSets(fullceds) == "sector"] <- "sectorGAINS"
 
-
-    # toolAggregate apparently gets confused if the mapping isn't
-    # subset exactly to the sectors present in the data. For
-    # some reason, just completing the data with complete_magpie
-    # is not enough.
-    # So here we loop through pollutant species and apply the mapping
-    # separately, accumulating them in a list before joining
-    # it all again
-    allspecies <- getItems(fullceds, "species")
-
-    # Store results in a list first, so the actual concatenation
-    # can be done in one go
-    listcedsgains <- list()
-    for (usespecies in allspecies) {
-      # Drop species dimension, add it back in the result later
-      useceds <- collapseDim(fullceds[, , usespecies])
-      usegains <- collapseDim(gainsemi[, , usespecies])
-
-      # Subset the mapping for that particular pollutant
-      usegainsmap <- gainsmap[gainsmap$CEDS62 %in% getItems(useceds, "sectorGAINS"), ]
-      usegainsmap <- gainsmap[gainsmap$GAINS2025 %in% getItems(usegains, "sectorGAINS"), ]
-
-      # And subset the arrays using the mapping
-      useceds <- useceds[, , unique(usegainsmap$CEDS62)]
-      usegains <- usegains[, , unique(usegainsmap$GAINS2025)]
-
-      # Apply mapping to GAINS sectors, using GAINS emissions as weights.
-      # As long as the mapping follows certain characteristics, mainly
-      # being able to be described fully using the INTERMEDIARY sectors
-      # in the mapping, this should ensure that:
-      # - Aggregating emissions follow CEDS
-      # - In each INTERMEDIARY secor, the proportion of GAINS emissions
-      #   is preserved in the final output
-      usecedsgains <- toolAggregate(
-        useceds,
-        usegainsmap[, c("CEDS62", "GAINS2025")],
-        from = "CEDS62", to = "GAINS2025",
-        weight = usegains,
+    if (subtype == "sectorsINTERMEDIARY") {
+      # For INTERMEDIARY sectors, the aggregation is a simple sum
+      intceds <- toolAggregate(
+        fullceds[, , unique(gainsmap$CEDS62)],
+        gainsmap[, c("CEDS62", "INTERMEDIARY")],
+        from = "CEDS62", to = "INTERMEDIARY",
         dim = 3.1,
-        wdim = 3.1,
-        zeroWeight = "allow"
+        weight = NULL
       )
 
-      # Add the pollutant species dimension
-      usecedsgains <- add_dimension(usecedsgains, 3.2, "species", usespecies)
+      # Ensure all pollutants have all the sectors, filled with zeros
+      intceds <- complete_magpie(intceds, fill = 0)
 
-      # Append to the list
-      listcedsgains[[length(listcedsgains) + 1]] <- usecedsgains
+      out <- setYears(intceds)
+      unit <- outunits
+      desc <- paste0("Emissions in year ", baseyear)
+    } else if (subtype == "sectorsGAINS2025") {
+      # For GAINS2025 sectors, the aggregation needs to use
+      # the GAINS emissions as weights to keep the proportions,
+      # and that leads to quite a few more subtleties
+
+      # toolAggregate apparently gets confused if the mapping isn't
+      # subset exactly to the sectors present in the data. For
+      # some reason, just completing the data with complete_magpie
+      # is not enough.
+      # So here we loop through pollutant species and apply the mapping
+      # separately, accumulating them in a list before joining
+      # it all again
+      allspecies <- getItems(fullceds, "species")
+
+      # Store results in a list first, so the actual concatenation
+      # can be done in one go
+      listcedsgains <- list()
+      for (usespecies in allspecies) {
+        # Drop species dimension, add it back in the result later
+        useceds <- collapseDim(fullceds[, , usespecies])
+        usegains <- collapseDim(gainsemi[, , usespecies])
+
+        # Subset the mapping for that particular pollutant
+        usegainsmap <- gainsmap[gainsmap$CEDS62 %in% getItems(useceds, "sectorGAINS"), ]
+        usegainsmap <- gainsmap[gainsmap$GAINS2025 %in% getItems(usegains, "sectorGAINS"), ]
+
+        # And subset the arrays using the mapping
+        useceds <- useceds[, , unique(usegainsmap$CEDS62)]
+        usegains <- usegains[, , unique(usegainsmap$GAINS2025)]
+
+        # Apply mapping to GAINS sectors, using GAINS emissions as weights.
+        # As long as the mapping follows certain characteristics, mainly
+        # being able to be described fully using the INTERMEDIARY sectors
+        # in the mapping, this should ensure that:
+        # - Aggregating emissions follow CEDS
+        # - In each INTERMEDIARY secor, the proportion of GAINS emissions
+        #   is preserved in the final output
+        usecedsgains <- toolAggregate(
+          useceds,
+          usegainsmap[, c("CEDS62", "GAINS2025")],
+          from = "CEDS62", to = "GAINS2025",
+          weight = usegains,
+          dim = 3.1,
+          wdim = 3.1,
+          zeroWeight = "allow"
+        )
+
+        # Add the pollutant species dimension
+        usecedsgains <- add_dimension(usecedsgains, 3.2, "species", usespecies)
+
+        # Append to the list
+        listcedsgains[[length(listcedsgains) + 1]] <- usecedsgains
+      }
+
+      # Concatenate all species in one go
+      fullcedsgains <- mbind(listcedsgains)
+
+      # Ensure all pollutants have all the sectors, filled with zeros
+      fullcedsgains <- complete_magpie(fullcedsgains, fill = 0)
+
+      out <- setYears(fullcedsgains)
+      unit <- outunits
+      desc <- paste0("Emissions in year ", baseyear)
     }
-
-    # Concatenate all species in one go
-    fullcedsgains <- mbind(listcedsgains)
-
-    # Ensure all pollutants have all the sectors, filled with zeros
-    fullcedsgains <- complete_magpie(fullcedsgains, fill = 0)
-
-    out <- setYears(fullcedsgains)
-    unit <- outunits
-    desc <- paste0("Emissions in year ", baseyear)
   }
 
 
