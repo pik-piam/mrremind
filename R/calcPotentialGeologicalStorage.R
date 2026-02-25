88d9d1a8a05e288e661d57abce1752beabaaaf0a
David Klein <dklein@pik-potsdam.de>
2026-02-25 10:09:37 +0100

    Remove 'source' switch

--- R/calcPotentialGeologicalStorage.R
+++ R/calcPotentialGeologicalStorage.R
@@ -12,37 +12,31 @@
 #' }
 #'
 
-calcPotentialGeologicalStorage <- function(source) {
+calcPotentialGeologicalStorage <- function() {
 
-  #if (source == "Gidden2025") {
-    # read geological storage potential from Gidden et al. 2025
-    new <- readSource(type = "Gidden2025_geological_storage_potential")
-    new <- new * 12/44 # GtCO2 -> GtC
+  # read geological storage potential from Gidden et al. 2025
+  new <- readSource(type = "Gidden2025_geological_storage_potential")
+  new <- new * 12/44 # GtCO2 -> GtC
 
-  #} else if (source == "MixedOld") {
-    # move content of calcLimitCCS.R here and delete it
-    # Read capacity factor inputs
-    data <- readSource("REMIND_11Regi", subtype = "ccs")
+  # move content of calcLimitCCS.R here and delete it
+  # Read capacity factor inputs
+  data <- readSource("REMIND_11Regi", subtype = "ccs")
 
-    # add dimensions that are the same for all regions but a dimension of the parameter in the GAMS code
-    getNames(data) <- "mixedOld"
-    #data <- add_dimension(data, dim = 3.2, add = "rlf", nm = "1")
+  # add dimensions that are the same for all regions but a dimension of the parameter in the GAMS code
+  getNames(data) <- "mixedOld"
+  #data <- add_dimension(data, dim = 3.2, add = "rlf", nm = "1")
 
-    # overwrite values for regions that belong to EUR and NEU
-    data_ccs_storage_potential <- readSource("CCS_StoragePotential")
-    eur.34 <- c("ALA", "AUT", "BEL", "BGR", "HRV", "CYP", "CZE", "DNK", "EST", "FRO",
-                "FIN", "FRA", "DEU", "GIB", "GRC", "GGY", "HUN", "IRL", "IMN", "ITA",
-                "JEY", "LVA", "LTU", "LUX", "MLT", "NLD", "POL", "PRT", "ROU", "SVK",
-                "SVN", "ESP", "SWE", "GBR")
-    non.eu <- c("ALB", "AND", "BIH", "GRL", "VAT", "ISL", "LIE", "MKD", "MCO", "MNE",
-                "NOR", "SMR", "SRB", "SJM", "CHE", "TUR")
-    overwrite <- c(eur.34, non.eu)
-    data[overwrite, , ] <- data_ccs_storage_potential[overwrite, , ]
-    old <- data
-
-  #} else {
-  #  stop("'", source, "' is an unknown source for calcPotentialGeologicalStorage")
-  #}
+  # overwrite values for regions that belong to EUR and NEU
+  data_ccs_storage_potential <- readSource("CCS_StoragePotential")
+  eur.34 <- c("ALA", "AUT", "BEL", "BGR", "HRV", "CYP", "CZE", "DNK", "EST", "FRO",
+              "FIN", "FRA", "DEU", "GIB", "GRC", "GGY", "HUN", "IRL", "IMN", "ITA",
+              "JEY", "LVA", "LTU", "LUX", "MLT", "NLD", "POL", "PRT", "ROU", "SVK",
+              "SVN", "ESP", "SWE", "GBR")
+  non.eu <- c("ALB", "AND", "BIH", "GRL", "VAT", "ISL", "LIE", "MKD", "MCO", "MNE",
+              "NOR", "SMR", "SRB", "SJM", "CHE", "TUR")
+  overwrite <- c(eur.34, non.eu)
+  data[overwrite, , ] <- data_ccs_storage_potential[overwrite, , ]
+  old <- data
 
   pot <- mbind(new, old)
 
