df75816e53ff3bddf1dd091d8476eeb80568e5ed
Falk Benke <falk.benke@pik-potsdam.de>
2026-03-13 11:34:17 +0100

    fix application of ESM mapping

--- R/calcIO.R
+++ R/calcIO.R
@@ -23,44 +23,52 @@
 #' @importFrom dplyr filter mutate
 calcIO <- function(subtype = c("input", "output", "output_biomass", "output_reporting", "trade"),
                    ieaVersion = "default", corrected = FALSE) {
-
   subtype <- match.arg(subtype)
 
-  switch(
-    subtype,
+  switch(subtype,
     input = {
-      mapping <- toolGetMapping(type = "sectoral",
-                                name = "structuremappingIO_inputs.csv",
-                                where = "mrremind",
-                                returnPathOnly = TRUE)
+      mapping <- toolGetMapping(
+        type = "sectoral",
+        name = "structuremappingIO_inputs.csv",
+        where = "mrremind",
+        returnPathOnly = TRUE
+      )
       target <- c("REMINDitems_in", "REMINDitems_out", "REMINDitems_tech")
     },
     output = {
-      mapping <- toolGetMapping(type = "sectoral",
-                                name = "structuremappingIO_outputs.csv",
-                                where = "mrcommons",
-                                returnPathOnly = TRUE)
+      mapping <- toolGetMapping(
+        type = "sectoral",
+        name = "structuremappingIO_outputs.csv",
+        where = "mrcommons",
+        returnPathOnly = TRUE
+      )
       target <- c("REMINDitems_in", "REMINDitems_out", "REMINDitems_tech")
     },
     output_biomass = {
-      mapping <- toolGetMapping(type = "sectoral",
-                                name = "structuremappingIO_outputs.csv",
-                                where = "mrcommons",
-                                returnPathOnly = TRUE)
+      mapping <- toolGetMapping(
+        type = "sectoral",
+        name = "structuremappingIO_outputs.csv",
+        where = "mrcommons",
+        returnPathOnly = TRUE
+      )
       target <- c("REMINDitems_in", "REMINDitems_out", "REMINDitems_tech")
     },
     output_reporting = {
-      mapping <- toolGetMapping(type = "sectoral",
-                                name = "structuremappingIO_outputs.csv",
-                                where = "mrcommons",
-                                returnPathOnly = TRUE)
+      mapping <- toolGetMapping(
+        type = "sectoral",
+        name = "structuremappingIO_outputs.csv",
+        where = "mrcommons",
+        returnPathOnly = TRUE
+      )
       target <- c("REMINDitems_in", "REMINDitems_out", "REMINDitems_tech")
     },
     trade = {
-      mapping <- toolGetMapping(type = "sectoral",
-                                name = "structuremappingIO_trade.csv",
-                                where = "mrremind",
-                                returnPathOnly = TRUE)
+      mapping <- toolGetMapping(
+        type = "sectoral",
+        name = "structuremappingIO_trade.csv",
+        where = "mrremind",
+        returnPathOnly = TRUE
+      )
       target <- c("REMINDitems_enty", "REMINDitems_trade")
     }
   )
@@ -82,10 +90,12 @@ calcIO <- function(subtype = c("input", "output", "output_biomass", "output_repo
   # add total buildings electricity demand (feelb = feelcb + feelhpb + feelrhb)
   # TODO: integrate in some mapping?
   if (subtype %in% c("output", "output_reporting")) {
-    ieamatch <- rbind(ieamatch,
-                      ieamatch %>%
-                        filter(.data$REMINDitems_out %in% c("feelcb", "feelhpb", "feelrhb")) %>%
-                        mutate(REMINDitems_out = "feelb"))
+    ieamatch <- rbind(
+      ieamatch,
+      ieamatch %>%
+        filter(.data$REMINDitems_out %in% c("feelcb", "feelhpb", "feelrhb")) %>%
+        mutate(REMINDitems_out = "feelb")
+    )
   }
 
   # filter items starting with x_, as they are not used in REMIND, but only for reporting
@@ -96,18 +106,21 @@ calcIO <- function(subtype = c("input", "output", "output_biomass", "output_repo
 
   # apply subsector mapping
   if (subtype %in% c("output", "output_reporting", "output_biomass")) {
-
-    subsectorMapping <- toolGetMapping(type = "sectoral",
-                                   name = "mappingIEA_EDGEsubsectors_to_ESOutput.csv",
-                                   where = "mrremind")
+    subsectorMapping <- toolGetMapping(
+      type = "sectoral",
+      name = "mappingIEA_EDGEsubsectors_to_ESOutput.csv",
+      where = "mrremind"
+    )
 
     ieamatch <- ieamatch %>%
       left_join(subsectorMapping, by = c("REMINDitems_in", "REMINDitems_out", "REMINDitems_tech")) %>%
-      select(-c("REMINDitems_in", "REMINDitems_out", "REMINDitems_tech")) %>%
-      rename("REMINDitems_in" = "REMINDitems_in_ESM", "REMINDitems_out" = "REMINDitems_out_ESM",
-             "REMINDitems_tech" = "REMINDitems_tech_ESM")
+      mutate(
+        "REMINDitems_in" = ifelse(is.na(.data$REMINDitems_in_ESM), .data$REMINDitems_in, .data$REMINDitems_in_ESM),
+        "REMINDitems_out" = ifelse(is.na(.data$REMINDitems_out_ESM), .data$REMINDitems_out, .data$REMINDitems_out_ESM),
+        "REMINDitems_tech_ESM" = ifelse(is.na(.data$REMINDitems_tech_ESM), .data$REMINDitems_tech, .data$REMINDitems_tech_ESM)
+      ) %>%
+      select(-c("REMINDitems_in_ESM", "REMINDitems_out_ESM", "REMINDitems_tech_ESM"))
   }
-
   ieamatch <- ieamatch %>%
     select(tidyselect::all_of(c("iea_product", "iea_flows", "Weight", target))) %>%
     stats::na.omit() %>%
@@ -115,81 +128,109 @@ calcIO <- function(subtype = c("input", "output", "output_biomass", "output_repo
     tidyr::unite("product.flow", c("iea_product", "iea_flows"), sep = ".") %>%
     dplyr::filter(.data$product.flow %in% getNames(data))
 
-  reminditems <-  do.call(
+  reminditems <- do.call(
     mbind,
-    lapply(unique(ieamatch$target),
-           function(item) {
-             product_flow <- ieamatch %>%
-               filter(item == .data$target) %>%
-               pull("product.flow")
+    lapply(
+      unique(ieamatch$target),
+      function(item) {
+        product_flow <- ieamatch %>%
+          filter(item == .data$target) %>%
+          pull("product.flow")
 
-             weights <- ieamatch %>%
-               filter(item == .data$target) %>%
-               pull("Weight") %>%
-               as.numeric()
+        weights <- ieamatch %>%
+          filter(item == .data$target) %>%
+          pull("Weight") %>%
+          as.numeric()
 
-             tmp <- dimSums(data[, , product_flow]
-                            * setNames(as.magpie(weights), product_flow),
-                            dim = 3, na.rm = TRUE)
-             getNames(tmp) <- item
+        tmp <- dimSums(
+          data[, , product_flow]
+          * setNames(as.magpie(weights), product_flow),
+          dim = 3, na.rm = TRUE
+        )
+        getNames(tmp) <- item
 
-             return(tmp)
-           })
+        return(tmp)
+      }
+    )
   )
 
   if (corrected) {
-
     if (subtype == "trade") {
       # adjust the inconsistent trade data from IEA for JPN
       reminditems["JPN", 2005, "peoil.Mport"] <- reminditems["JPN", 2005, "peoil.Mport"] - 0.0245 / 31.71e-03
     }
 
     if (subtype %in% c("output", "input", "output_reporting")) {
-
       # Split residential Biomass into traditional and modern biomass depending upon the income per capita ----
 
       # In order to split the REMIND technology biotr between biotr and biotrmod,
       # We use the traditional biomass split for EDGE buildings and divide by the total quantity of FE biomass
 
-      edgeBio <- calcOutput("IOEdgeBuildings", subtype = "output_EDGE_buildings",
-                            ieaVersion = ieaVersion, aggregate = FALSE)
+      edgeBio <- calcOutput("IOEdgeBuildings",
+        subtype = "output_EDGE_buildings",
+        ieaVersion = ieaVersion, aggregate = FALSE
+      )
 
       # TODO: can we avoid this recursive calculation?
       feBio <- calcOutput("IO", subtype = "output_biomass", ieaVersion = ieaVersion, aggregate = FALSE)
       shareBiotrad <- edgeBio[, , "biotrad"] / (feBio[, , "sesobio.fesob.tdbiosob"] + feBio[, , "sesobio.fesoi.tdbiosoi"])
       shareBiotrad[is.na(shareBiotrad)] <- 0
-      reminditems <- mbind(reminditems,
-                           setNames(reminditems[, , "pebiolc.sesobio.biotr"] * (1 - shareBiotrad),
-                                    "pebiolc.sesobio.biotrmod"))
+      reminditems <- mbind(
+        reminditems,
+        setNames(
+          reminditems[, , "pebiolc.sesobio.biotr"] * (1 - shareBiotrad),
+          "pebiolc.sesobio.biotrmod"
+        )
+      )
       reminditems[, , "pebiolc.sesobio.biotr"] <- reminditems[, , "pebiolc.sesobio.biotr"] * (shareBiotrad)
 
       # replace IEA data for 1st generation biomass with data that also MAgPIE uses ----
       bio1st <- calcOutput("1stBioDem", subtype = "ethanol_oils", aggregate = FALSE) / 1000 # PJ to EJ
       reminditems[, , "pebios.seliqbio.bioeths"] <-
-        time_interpolate(bio1st[, , "pebios"], interpolated_year = getYears(reminditems),
-                         integrate_interpolated_years = FALSE, extrapolation_type = "constant")
+        time_interpolate(bio1st[, , "pebios"],
+          interpolated_year = getYears(reminditems),
+          integrate_interpolated_years = FALSE, extrapolation_type = "constant"
+        )
       reminditems[, , "pebioil.seliqbio.biodiesel"] <-
-        time_interpolate(bio1st[, , "pebioil"], interpolated_year = getYears(reminditems),
-                         integrate_interpolated_years = FALSE, extrapolation_type = "constant")
+        time_interpolate(bio1st[, , "pebioil"],
+          interpolated_year = getYears(reminditems),
+          integrate_interpolated_years = FALSE, extrapolation_type = "constant"
+        )
 
       # re-calculating fepet and fedie final energy based on updated EDGE shares ----
       share <- calcOutput("LDVShares", warnNA = FALSE, aggregate = FALSE)
       regions <- getItems(share, dim = 1.1)
-      feShares <- new.magpie(cells_and_regions = regions, years = intersect(getYears(share), getYears(reminditems)),
-                             names = c("seliqfos.fepet.tdfospet", "seliqbio.fepet.tdbiopet", "seliqfos.fedie.tdfosdie",
-                                       "seliqbio.fedie.tdbiodie"))
-      feShares[regions, getYears(feShares), "fepet"] <- setNames(share[regions, getYears(feShares), "share_LDV_totliq"],
-                                                                 NULL)
+      feShares <- new.magpie(
+        cells_and_regions = regions, years = intersect(getYears(share), getYears(reminditems)),
+        names = c(
+          "seliqfos.fepet.tdfospet", "seliqbio.fepet.tdbiopet", "seliqfos.fedie.tdfosdie",
+          "seliqbio.fedie.tdbiodie"
+        )
+      )
+      feShares[regions, getYears(feShares), "fepet"] <- setNames(
+        share[regions, getYears(feShares), "share_LDV_totliq"],
+        NULL
+      )
       feShares[regions, getYears(feShares), "fedie"] <-
         (1 - setNames(share[regions, getYears(feShares), "share_LDV_totliq"], NULL))
-      feTotal <- dimSums(reminditems[regions, getYears(feShares),
-                                     c("seliqfos.fepet.tdfospet", "seliqbio.fepet.tdbiopet", "seliqfos.fedie.tdfosdie",
-                                       "seliqbio.fedie.tdbiodie")],
-                         dim = c(3.2, 3.3)) # seliqfos fedie dot
-      feTransp <- new.magpie(cells_and_regions = regions,
-                             years = getYears(feShares),
-                             names = c("seliqfos.fepet.tdfospet", "seliqbio.fepet.tdbiopet", "seliqfos.fedie.tdfosdie",
-                                       "seliqbio.fedie.tdbiodie"))
+      feTotal <- dimSums(
+        reminditems[
+          regions, getYears(feShares),
+          c(
+            "seliqfos.fepet.tdfospet", "seliqbio.fepet.tdbiopet", "seliqfos.fedie.tdfosdie",
+            "seliqbio.fedie.tdbiodie"
+          )
+        ],
+        dim = c(3.2, 3.3)
+      ) # seliqfos fedie dot
+      feTransp <- new.magpie(
+        cells_and_regions = regions,
+        years = getYears(feShares),
+        names = c(
+          "seliqfos.fepet.tdfospet", "seliqbio.fepet.tdbiopet", "seliqfos.fedie.tdfosdie",
+          "seliqbio.fedie.tdbiodie"
+        )
+      )
       feTransp[, , "seliqfos.fepet.tdfospet"] <-
         feShares[, , "seliqfos.fepet.tdfospet"] * setNames(feTotal[, , "seliqfos"], "seliqfos.fepet.tdfospet")
       feTransp[, , "seliqbio.fepet.tdbiopet"] <-
@@ -198,19 +239,24 @@ calcIO <- function(subtype = c("input", "output", "output_biomass", "output_repo
         feShares[, , "seliqfos.fedie.tdfosdie"] * setNames(feTotal[, , "seliqfos"], "seliqfos.fedie.tdfosdie")
       feTransp[, , "seliqbio.fedie.tdbiodie"] <-
         feShares[, , "seliqbio.fedie.tdbiodie"] * setNames(feTotal[, , "seliqbio"], "seliqbio.fedie.tdbiodie")
-      reminditems[, getYears(feTransp), c("seliqfos.fepet.tdfospet", "seliqbio.fepet.tdbiopet",
-                                          "seliqfos.fedie.tdfosdie", "seliqbio.fedie.tdbiodie")] <-
-        feTransp[, , c("seliqfos.fepet.tdfospet", "seliqbio.fepet.tdbiopet", "seliqfos.fedie.tdfosdie",
-                       "seliqbio.fedie.tdbiodie")]
+      reminditems[, getYears(feTransp), c(
+        "seliqfos.fepet.tdfospet", "seliqbio.fepet.tdbiopet",
+        "seliqfos.fedie.tdfosdie", "seliqbio.fedie.tdbiodie"
+      )] <-
+        feTransp[, , c(
+          "seliqfos.fepet.tdfospet", "seliqbio.fepet.tdbiopet", "seliqfos.fedie.tdfosdie",
+          "seliqbio.fedie.tdbiodie"
+        )]
     }
   }
 
-  desc <- paste0("IEA SE Output Data based on ", toolGetIEAYear(ieaVersion), " edition of IEA World Energy Balances")
+  desc <- paste0("IEA SE Output Data based on ", toolGetIEAYear(ieaVersion),
+                 " edition of IEA World Energy Balances")
 
   return(list(
     x = reminditems,
     weight = NULL,
     unit = "EJ",
-    description = desc)
-  )
+    description = desc
+  ))
 }
