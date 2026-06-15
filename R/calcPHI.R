92f01489d842548c2c1249095ed56019466aee7e
Adamantios Koutsandreas <adamanti@LT1253.pik-potsdam.de>
2026-06-15 14:23:35 +0200

    Add PHI, WACC and cost of capital files

---
+++ R/calcPHI.R
@@ -0,0 +1,18 @@
+#' Provides REMIND data for realization Investment_Inefficiencies in 01_macro Module
+#'
+#' @author Diamantis Koutsandreas
+#'
+#'
+calcPHI <- function() {
+
+  output <- readSource("IMF_PHI")
+
+  return(
+    list(
+      x = output,
+      unit = "%",
+      description = "share of investments in each region that are not transformed into productive capital",
+      isocountries = FALSE
+      )
+  )
+}
---
+++ R/readETH_WACC.R
@@ -0,0 +1,30 @@
+#' This function reads WACC markup data across technologies for each of
+#' the REMIND regions
+#' @author Diamantis Koutsandreas
+
+#' @noRd
+utils::globalVariables(c("value", "min_val", "reg", "tewacc"))
+
+readETH_WACC <- function() {
+  years <- c(2010, 2015, 2020, 2025, 2030, 2035, 2040, 2045, 2050,
+             2055, 2060, 2070, 2080, 2090, 2100, 2110, 2130, 2150)
+
+  data <- readxl::read_xlsx("Source_data_WACC.xlsx",
+                            sheet = "wacc_markups",
+                            range = "A1:CH13") %>%
+    pivot_longer(cols = 2:86, names_to = "tewacc", values_to = "value") %>%
+    rename(reg = 1) # Rename the region column to 'reg'
+
+  data_expanded <- data %>%
+    slice(rep(seq_len(dplyr::n()), each = length(years))) %>%
+    mutate(t = rep(years, times = nrow(data)))
+
+  data_expanded <- data_expanded %>%
+    select("t", "reg", "tewacc", "value")
+
+  return(as.magpie(data_expanded))
+}
+
+
+
+
